b7599e6a8be8830e3109bca62b197745ec43534e
Björn Eyselein <bjoern.eyselein@stud-mail.uni-wuerzburg.de>
2015-11-09 15:12:35 +0100

    ExcelController: Logincheck eingefügt und Dateiuploaddummy erstellt

--- app/controllers/Excel.java
+++ app/controllers/Excel.java
@@ -1,11 +1,15 @@
 package controllers;
 
+import java.nio.file.Path;
+
 import model.Student;
 import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
 import views.html.excel.excel;
-import views.html.excel.exceloverview;
 import views.html.excel.excelcorrect;
+import views.html.excel.exceloverview;
 
 public class Excel extends Controller {
   
@@ -34,12 +38,35 @@ public class Excel extends Controller {
   }
   
   public Result upload() {
-    String userName = session("id");
-    if(userName == null)
+    if(session(Application.SESSION_ID_FIELD) == null)
       return redirect("/login");
-    Student user = Student.find.byId(userName);
+    Student user = Student.find.byId(session(Application.SESSION_ID_FIELD));
+    if(user == null) {
+      session().clear();
+      return redirect("/login");
+    }
     
-    return ok(excelcorrect.render(user));
+    MultipartFormData body = request().body().asMultipartFormData();
+    FilePart htmlFile = body.getFile("solFile");
+    if(htmlFile != null) {
+      Path path = htmlFile.getFile().toPath();
+      System.out.println(path);
+      // FIXME: Save file with correct name!
+      
+      // List<String> fileContent = Files.readAllLines(file);
+      // saveSolutionForUser(user.name, String.join("\n", fileContent),
+      // exercise);
+      //
+      // String url = "/solutions/" + user.name + "/html/" + exercise;
+      // List<ElementResult> result = HtmlCorrector.correct(url, ex, user);
+      //
+      // List<String> solution =
+      // Files.readAllLines(Util.getSolFileForExercise(user.name, exercise));
+      
+      return ok(excelcorrect.render(user));
+    } else {
+      return badRequest("Datei konnte nicht hochgeladen werden!");
+    }
   }
   
 }
