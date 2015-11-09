00dc15f1f8d3dedcc5c0628bb075947a1224814e
Björn Eyselein <bjoern.eyselein@stud-mail.uni-wuerzburg.de>
2015-11-09 10:47:38 +0100

    Übersichtsseite für Nutzer hinzugefügt

---
+++ app/controllers/User.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import java.util.List;
+
+import model.Grading;
+import model.Student;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.user;
+
+public class User extends Controller {
+  
+  public Result index() {
+    if(session(Application.SESSION_ID_FIELD) == null)
+      return redirect("/login");
+    Student student = Student.find.byId(session(Application.SESSION_ID_FIELD));
+    
+    List<Grading> gradings = Grading.finder.where().eq("student_name", student.name).findList();
+    
+    return ok(user.render("User", student, gradings));
+  }
+  
+}
