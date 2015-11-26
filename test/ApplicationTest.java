b7aaa7499eae742448e442d3602ca98187d7f1ac
Björn Eyselein <bjoern.eyselein@stud-mail.uni-wuerzburg.de>
2015-11-26 11:13:34 +0100

    Formatierungsänderung noch nicht implementierte Tests

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -2,24 +2,22 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-
 /**
-*
-* Simple (JUnit) tests that can call all parts of a play app.
-* If you are interested in mocking a whole application, see the wiki for more details.
-*
-*/
+ *
+ * Simple (JUnit) tests that can call all parts of a play app. If you are
+ * interested in mocking a whole application, see the wiki for more details.
+ *
+ */
 public class ApplicationTest {
-
-    @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertEquals(2, a);
-    }
-
-    @Test
-    public void renderTemplate() {
-    }
-
-
+  
+  @Test
+  public void simpleCheck() {
+    int a = 1 + 1;
+    assertEquals(2, a);
+  }
+  
+  @Test
+  public void renderTemplate() {
+  }
+  
 }
