7524ad280c6701046fa93d8c235cf8dedef02d45
Björn Eyselein <bjoern.eyselein@stud-mail.uni-wuerzburg.de>
2015-12-21 12:43:47 +0100

    Farbe für teilweise richtige Aufgabe von blau auf gelb geändert

--- app/controllers/exercises/JavaScript.java
+++ app/controllers/exercises/JavaScript.java
@@ -1,12 +1,16 @@
 package controllers.exercises;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+import model.javascript.JavascriptExercise;
+import model.javascript.JavascriptTest;
 import model.user.Student;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -18,40 +22,59 @@ import views.html.javascript.*;
 @Security.Authenticated(Secured.class)
 public class JavaScript extends Controller {
   
-  private static String[][] testValues = {{"1", "1", "2"}, {"1", "2", "3"}, {"4", "7", "11"}, {"83", "74", "157"}};
-  private static String exerciseText = "Implementieren Sie folgende Funktion 'sum', die zwei Zahlen entegennimmt und deren Summe zurückgibt.";
-  private static final String exercise = "function sum(a, b) {\n  return 0;\n}";
+  // private static String[][] testValues = {{"1", "1", "2"}, {"1", "2", "3"},
+  // {"2", "2", "4"}, {"4", "7", "11"},
+  // {"83", "74", "157"}};
+  // private static String exerciseText =
+  // "Implementieren Sie folgende Funktion 'sum', die zwei Zahlen entegennimmt und deren Summe zurückgibt.";
+  // private static final String exercise =
+  // "function sum(a, b) {\n  return 0;\n}";
   
   public Result commit() {
-    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-    
-    LinkedList<String> testResults = new LinkedList<String>();
-    
     Map<String, String[]> body = request().body().asFormUrlEncoded();
     String learnerSolution = body.get("editorContent")[0];
     
-    try {
-      engine.eval(learnerSolution);
-      
-      for(String[] row: testValues) {
-        String summand1 = row[0], summand2 = row[1], summe = row[2];
-        String addition = "sum(" + summand1 + ", " + summand2 + ");";
-        String ergebnis;
-        ergebnis = engine.eval(addition).toString();
-        
-        if(ergebnis.equals(summe))
-          testResults.add("Test von " + addition + " war erfolgreich!");
-        else
-          testResults.add("Ergebnis von " + addition + " war " + engine.eval(addition) + ", erwartet wurde " + summe);
-      }
-    } catch (ScriptException e) {
-      e.printStackTrace();
-    }
+    LinkedList<String> testResults = correct(learnerSolution);
+    
     return ok(javascriptcorrect.render(learnerSolution, testResults,
         Student.find.byId(session(Application.SESSION_ID_FIELD))));
   }
   
+  private LinkedList<String> correct(String learnerSolution) {
+    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+    LinkedList<String> testResults = new LinkedList<String>();
+    
+    try {
+      // Lese programmierte Lernerlösung ein
+      engine.eval(learnerSolution);
+      
+      // Evaluiere Lernerlösung mit Testwerten
+      JavascriptExercise<Integer, Long> exercise = new JavascriptExercise.IntegerStandardTest();
+      for(JavascriptTest<Integer, Long> test: exercise.getTests()) {
+        // TODO: testing!
+        List<String> values = test.getTestValues().stream().map(value -> {
+          return value.toString();
+        }).collect(Collectors.toList());
+        String testValues = String.join(", ", values);
+        Long ergebnis = (Long) engine.eval("sum(" + testValues + ");");
+        
+        if(ergebnis.equals(test.getRealResult()))
+          testResults.add("Test von " + test.toString() + " war erfolgreich!");
+        else
+          testResults.add("Ergebnis von " + test.toString() + " war " + ergebnis + ", erwartet wurde "
+              + test.getRealResult());
+      }
+      
+    } catch (ScriptException e) {
+      e.printStackTrace();
+    }
+    return testResults;
+  }
+  
   public Result index() {
-    return ok(javascript.render(Student.find.byId(session(Application.SESSION_ID_FIELD)), exerciseText, exercise));
+    return ok(javascript.render(Student.find.byId(session(Application.SESSION_ID_FIELD)), "", ""));
+    // return
+    // ok(javascript.render(Student.find.byId(session(Application.SESSION_ID_FIELD)),
+    // exerciseText, exercise));
   }
 }
