85f6171633234f8ce8c7214964fed38c4bb82e1f
Björn Eyselein <bjoern.eyselein@stud-mail.uni-wuerzburg.de>
2016-02-22 10:49:16 +0100

    Mindmap weitergeführt

--- modules/mindmap/app/controllers/mindmap/Mindmap.java
+++ modules/mindmap/app/controllers/mindmap/Mindmap.java
@@ -1,21 +1,23 @@
 package controllers.mindmap;
 
-import model.user.Student;
-import model.user.User;
+import model.user.Secured;
 import model.user.UserControl;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security;
 import views.html.mindmapindex;
+import views.html.mindmapcorrect;
 
+@Security.Authenticated(Secured.class)
 public class Mindmap extends Controller {
   
   public Result index() {
-//    return ok("TODO");
     return ok(mindmapindex.render(UserControl.getUser()));
   }
   
-  public Result test() {
-    return badRequest("****");
+  public Result upload() {
+    // TODO: getFile, correct and present for download!
+    return ok(mindmapcorrect.render(UserControl.getUser()));
   }
   
 }
