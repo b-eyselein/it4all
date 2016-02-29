d61250f64016fa7c42e6afa549ffe3f1be9a750c
Björn Eyselein <bjoern.eyselein@stud-mail.uni-wuerzburg.de>
2016-02-29 13:01:06 +0100

    Kleines Refactoring Html

--- modules/web/app/controllers/web/HTML.java
+++ modules/web/app/controllers/web/HTML.java
@@ -13,15 +13,12 @@ import model.html.HtmlExercise;
 import model.user.Secured;
 import model.user.User;
 import play.mvc.Controller;
-import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
 import play.mvc.Security;
-import play.twirl.api.Html;
 import views.html.html.html;
 import views.html.html.htmlcorrect;
 import views.html.html.htmloverview;
-//import controllers.Application;
 import controllers.core.UserControl;
 import controllers.core.Util;
 
@@ -29,22 +26,15 @@ public class HTML extends Controller {
   
   @Security.Authenticated(Secured.class)
   public Result exercise(int exercise) {
-    User student = UserControl.getUser();
-    if(student == null) {
-      session().clear();
-      return redirect("/login");
-    }
     HtmlExercise exer = HtmlExercise.finder.byId(exercise);
     if(exer == null)
       return redirect("/html/");
-    return ok(html.render(student, exer));
+    return ok(html.render(UserControl.getUser(), exer));
   }
   
   @Security.Authenticated(Secured.class)
   public Result index() {
-    User student = UserControl.getUser();
-    List<HtmlExercise> exercises = HtmlExercise.finder.all();
-    return ok(htmloverview.render(exercises, student));
+    return ok(htmloverview.render(HtmlExercise.finder.all(), UserControl.getUser()));
   }
   
   @Security.Authenticated(Secured.class)
@@ -60,46 +50,41 @@ public class HTML extends Controller {
       Path saveTo = Util.getHtmlSolFileForExercise(user, "html", exercise);
       Files.write(saveTo, Arrays.asList(solution), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
     } catch (IOException e) {
+      // TODO: Log exception!
       System.out.println(e);
     }
   }
   
   public Result site(String userName, int exercise) {
-    List<String> strings = Arrays.asList("Es gab einen Fehler...");
-    Path path = Util.getHtmlSolFileForExercise(userName, "html", exercise);
-    if(Files.exists(path)) {
-      try {
-        strings = Files.readAllLines(path);
-        for(String s: strings)
-          s.trim();
-      } catch (IOException e) {
-      }
-    }
-    return ok(new Html(String.join("\n", strings)));
+    Path file = Util.getHtmlSolFileForExercise(userName, "html", exercise);
+    if(Files.exists(file))
+      return ok(file.toFile());
+    else
+      return badRequest("Fehler: Datei nicht vorhanden!");
   }
   
   @Security.Authenticated(Secured.class)
-  public Result upload(int exercise) {
+  public Result upload(int exerciseId) {
     User user = UserControl.getUser();
-    HtmlExercise ex = HtmlExercise.finder.byId(exercise);
-    MultipartFormData body = request().body().asMultipartFormData();
-    FilePart htmlFile = body.getFile("solFile");
-    if(htmlFile != null) {
-      try {
-        Path file = htmlFile.getFile().toPath();
-        List<String> fileContent = Files.readAllLines(file);
-        saveSolutionForUser(user.getName(), String.join("\n", fileContent), exercise);
-        
-        String url = "/web/solutions/" + user.getName() + "/html/" + exercise;
-        List<ElementResult> result = HtmlCorrector.correct(url, ex, user);
-        
-        List<String> solution = Files.readAllLines(Util.getHtmlSolFileForExercise(user.getName(), "html", exercise));
-        
-        return ok(htmlcorrect.render(user, ex, result, solution));
-      } catch (IOException e) {
-        return badRequest("Datei konnte nicht hochgeladen werden!");
-      }
-    } else
+    HtmlExercise exercise = HtmlExercise.finder.byId(exerciseId);
+    
+    FilePart htmlFile = request().body().asMultipartFormData().getFile("solFile");
+    if(htmlFile == null)
       return badRequest("Datei konnte nicht hochgeladen werden!");
+    
+    try {
+      Path file = htmlFile.getFile().toPath();
+      List<String> fileContent = Files.readAllLines(file);
+      saveSolutionForUser(user.getName(), String.join("\n", fileContent), exerciseId);
+      
+      String url = "/web/solutions/" + user.getName() + "/html/" + exerciseId;
+      List<ElementResult> result = HtmlCorrector.correct(url, exercise, user);
+      
+      List<String> solution = Files.readAllLines(Util.getHtmlSolFileForExercise(user.getName(), "html", exerciseId));
+      
+      return ok(htmlcorrect.render(user, exercise, result, solution));
+    } catch (IOException e) {
+      return badRequest("Datei konnte nicht hochgeladen werden!");
+    }
   }
 }
