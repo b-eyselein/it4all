3f356387424ebb03e21370edcd20d8e520637cee
Björn Eyselein <bjoern.eyselein@stud-mail.uni-wuerzburg.de>
2016-03-22 14:42:32 +0100

    Kleine Änderungen

--- modules/web/app/controllers/web/HTML.java
+++ modules/web/app/controllers/web/HTML.java
@@ -15,7 +15,6 @@ import model.html.task.Task;
 import model.user.Secured;
 import model.user.User;
 import play.mvc.Controller;
-import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
 import play.mvc.Security;
 import play.mvc.Http.Request;
@@ -26,21 +25,21 @@ import controllers.core.UserControl;
 import controllers.core.Util;
 
 public class HTML extends Controller {
-  
+
   private static String serverUrl = Util.getServerUrl();
   private static String learnerSolValue = "editorContent";
-  
+
   @Security.Authenticated(Secured.class)
   public Result commit(int exerciseId) {
     User user = UserControl.getUser();
-    
+
     String learnerSolution = extractLearnerSolutionFromRequest(request(), learnerSolValue);
     saveSolutionForUser(user.getName(), learnerSolution, exerciseId);
-    
+
     String json = correctExercise(user, HtmlExercise.finder.byId(exerciseId));
     return ok(json).as("application/json");
   }
-  
+
   @Security.Authenticated(Secured.class)
   public Result exercise(int exerciseId) {
     HtmlExercise exercise = HtmlExercise.finder.byId(exerciseId);
@@ -49,12 +48,12 @@ public class HTML extends Controller {
     else
       return ok(html.render(UserControl.getUser(), exercise, serverUrl));
   }
-  
+
   @Security.Authenticated(Secured.class)
   public Result index() {
     return ok(htmloverview.render(HtmlExercise.finder.all(), UserControl.getUser()));
   }
-  
+
   public Result site(String userName, int exercise) {
     Path file = Util.getHtmlSolFileForExercise(userName, "html", exercise);
     if(Files.exists(file))
@@ -66,34 +65,34 @@ public class HTML extends Controller {
         // --> Evtl. entsprechend Fehler werfen bzw. in Korrektor auffangen!
         return badRequest("Fehler beim Lesen der Datei!");
       }
-    
+
     else
       return badRequest("Fehler: Datei nicht vorhanden!");
   }
-  
+
   private String correctExercise(User user, HtmlExercise exercise) {
     // FIXME: implement!
     String url = "/web/solutions/" + user.getName() + "/html/" + exercise.id;
     List<ElementResult<? extends Task>> result = HtmlCorrector.correct(url, exercise, user);
-    
+
     List<String> results = result.stream().map(res -> res.toJSON()).collect(Collectors.toList());
-    
+
     return "{\"results\": [\n\t" + String.join(",\n\t", results) + "\n]}";
   }
-  
+
   private String extractLearnerSolutionFromRequest(Request request, String value) {
     return request.body().asFormUrlEncoded().get(value)[0];
   }
-  
+
   private void saveSolutionForUser(String userName, String solution, int exercise) {
     try {
       if(!Files.exists(Util.getSolDirForUser(userName)))
         Files.createDirectory(Util.getSolDirForUser(userName));
-      
+
       Path solDir = Util.getSolDirForUserAndType("html", userName);
       if(!Files.exists(solDir))
         Files.createDirectory(solDir);
-      
+
       Path saveTo = Util.getHtmlSolFileForExercise(userName, "html", exercise);
       Files.write(saveTo, Arrays.asList(solution), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
     } catch (IOException e) {
--- modules/web/app/model/html/HtmlExercise.java
+++ modules/web/app/model/html/HtmlExercise.java
@@ -3,6 +3,7 @@ package model.html;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
@@ -17,21 +18,21 @@ import com.avaje.ebean.Model;
 @Entity
 @Table(name = "htmlexercise")
 public class HtmlExercise extends Model {
-  
+
   public static Finder<Integer, HtmlExercise> finder = new Finder<Integer, HtmlExercise>(HtmlExercise.class);
-  
+
   @Id
   public int id;
-  
+
   @Required
   public String title;
-  
+
   @Required
   public String text;
-  
-  @OneToMany(mappedBy = "exercise")
-  public List<Task> tasks;
 
+  @OneToMany(mappedBy = "exercise", cascade = CascadeType.ALL)
+  public List<Task> tasks;
+  
   public List<ElementResult<? extends Task>> getElementResults() {
     // FIXME: implement!
     return tasks.parallelStream().map(task -> task.getElementResult()).collect(Collectors.toList());
