1f45dba3526c81cdb321bcb58edfd97c5970ae76
Björn Eyselein <bjoern.eyselein@stud-mail.uni-wuerzburg.de>
2016-03-23 13:39:28 +0100

    Comment

--- modules/web/app/model/html/result/MultiNameResult.java
+++ modules/web/app/model/html/result/MultiNameResult.java
@@ -12,74 +12,74 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class MultiNameResult extends ElementResultWithAttributes<MultiNameTask> {
-  
+
   private List<String> defining_Attributes = new LinkedList<String>();
   private HashMap<String, WebElement> singleResults;
-  
+
   public MultiNameResult(MultiNameTask task, String commonAttributes, String differentAttributes) {
     super(task);
-    
+
     for(String attribute: differentAttributes.split(";"))
       if(attribute.contains("="))
         defining_Attributes.add(attribute);
-    
+
     // TODO: Defining Attributes must not be empty (or size >= 2?
     // [___MULTI___ElementResult])
     if(defining_Attributes.size() < 2)
       throw new IllegalArgumentException("Es müssen mindestens 2 definierende Attribute vorhanden sein!");
-    
+
     singleResults = new HashMap<String, WebElement>();
   }
-  
+
   @Override
   public Success evaluate(WebDriver driver) {
     List<WebElement> foundElements = driver.findElements(By.name(task.elemName));
     if(foundElements.isEmpty())
       return Success.NONE;
-    
+
     foundElements = filterForTagName(foundElements, task.tagName);
     if(foundElements.isEmpty())
       return Success.NONE;
-    
+
     // TODO: Stelle sicher, dass alle gefundenen Elemente alle Common-Attribute
     // besitzen
     for(WebElement element: foundElements)
       checkAttributes(element);
-    
+
     // Stelle sicher, dass alle Elemente jeweils ein Different-Attribut
     // enthalten
-    for(String attribute: defining_Attributes) {
-      // for(WebElement element: foundElements) {
-      // String key = attribute.split("=")[0], value = attribute.split("=")[1];
-      // AttributeResult attributeResult = new AttributeResult(element, key,
-      // value);
-      // if(attributeResult.isFound()) {
-      // attrs.add(new AttributeResult(element, key, value));
-      // singleResults.put(attribute, element);
-      // }
-      // }
-    }
-    
+    // for(String attribute: defining_Attributes) {
+    // for(WebElement element: foundElements) {
+    // String key = attribute.split("=")[0], value = attribute.split("=")[1];
+    // AttributeResult attributeResult = new AttributeResult(element, key,
+    // value);
+    // if(attributeResult.isFound()) {
+    // attrs.add(new AttributeResult(element, key, value));
+    // singleResults.put(attribute, element);
+    // }
+    // }
+    // }
+
     if(singleResults.size() == defining_Attributes.size())
       return Success.COMPLETE;
     else if(singleResults.size() > 0)
       return Success.PARTIALLY;
     else
       return Success.NONE;
-    
+
   }
-  
+
   @Override
   protected List<String> getAttributesAsJson() {
     // TODO Auto-generated method stub
     List<String> retList = new LinkedList<String>();
     return retList;
   }
-  
+
   @Override
   protected List<String> getMessagesAsJson() {
     // TODO Auto-generated method stub
     return Collections.emptyList();
   }
-  
+
 }
