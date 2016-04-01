8b589ac9de386bc0160d159a74aa7d302bbcbbf4
Björn Eyselein <bjoern.eyselein@stud-mail.uni-wuerzburg.de>
2016-04-01 11:27:13 +0200

    SQL-Link in main eingefügt

--- modules/sql/app/controllers/sql/SQL.java
+++ modules/sql/app/controllers/sql/SQL.java
@@ -9,36 +9,44 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import controllers.core.UserControl;
+import model.user.Secured;
+import model.user.User;
 import play.db.Database;
 import play.db.NamedDatabase;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security.Authenticated;
+import controllers.core.UserControl;
 import views.html.sql;
 
+@Authenticated(Secured.class)
 public class SQL extends Controller {
-  
+
+  private static final String DB_BASENAME = "sql_";
+
   @Inject
   @NamedDatabase("sqltest")
   Database db;
-  
+
   public Result index() {
-    
+
+    User user = UserControl.getUser();
+
     String sqlStatement = "select * from task";
-    
+
     try {
       Connection connection = db.getConnection();
-      
+
       // Change db to users own db
-      connection.setCatalog("sql_s319286");
-      
+      connection.setCatalog(DB_BASENAME + user.getName());
+
       // Execute query
       ResultSet resultSet = connection.createStatement().executeQuery(sqlStatement);
       ResultSetMetaData metadata = resultSet.getMetaData();
-      
+
       // Syso result of query
       List<List<String>> result = new LinkedList<List<String>>();
-      
+
       while(resultSet.next()) {
         List<String> row = new LinkedList<String>();
         for(int columnCount = 1; columnCount <= metadata.getColumnCount(); columnCount++)
@@ -47,10 +55,10 @@ public class SQL extends Controller {
       }
       connection.close();
       return ok(sql.render(result, UserControl.getUser()));
-      
+
     } catch (SQLException e) {
       return badRequest("Fehler bei Verarbeitung: " + e.getMessage() + "!");
     }
-    
+
   }
 }
