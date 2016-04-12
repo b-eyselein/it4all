81c54b215309835938786fb4849e295e317bf36b
Björn Eyselein <bjoern.eyselein@stud-mail.uni-wuerzburg.de>
2016-04-12 12:29:37 +0200

    CSS-View erstellt

--- modules/web/app/controllers/web/CSS.java
+++ modules/web/app/controllers/web/CSS.java
@@ -1,15 +1,18 @@
 package controllers.web;
 
+import java.util.Arrays;
+
+import controllers.core.UserControl;
 import model.user.Secured;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
+import views.html.css.cssOverview;
 
 @Security.Authenticated(Secured.class)
 public class CSS extends Controller {
-  
+
   public Result index() {
-    return ok("TEST...");
+    return ok(cssOverview.render(Arrays.asList("TEST...", "Test..."), UserControl.getUser()));
   }
-  
 }
