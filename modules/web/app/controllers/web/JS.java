88a7ac16dcb59f51e9023bf0c016a067dd7accd8
Björn Eyselein <bjoern.eyselein@stud-mail.uni-wuerzburg.de>
2016-04-18 14:58:28 +0200

    Vereinheitlichung Layout und Inhalt Web-Korrektur

--- modules/web/app/controllers/web/JS.java
+++ modules/web/app/controllers/web/JS.java
@@ -23,30 +23,42 @@ import model.user.Secured;
 
 @Security.Authenticated(Secured.class)
 public class JS extends Controller {
-  
+
   private static String serverUrl = Util.getServerUrl();
-  
+
   public Result commit(int exerciseId) {
     Map<String, String[]> body = request().body().asFormUrlEncoded();
     String learnerSolution = body.get("editorContent")[0];
-    
+
     List<JsTestResult> testResults = correct(JsExercise.finder.byId(exerciseId), learnerSolution);
-    
+
     List<String> results = testResults.stream().map(res -> res.getAsString()).collect(Collectors.toList());
     return ok(String.join("\n", results));
-    
+
     // TODO: Wird jscorrecot.scala.html gebraucht? --> Nur für Endkorrektur ?!?
     // return ok(jscorrect.render(learnerSolution, testResults,
     // Application.getUser()));
   }
-  
+
+  public Result exercise(int id) {
+    if(JsExercise.finder.byId(id) != null)
+      // TODO: Url for upload!
+      return ok(js.render(UserControl.getUser(), JsExercise.finder.byId(id), serverUrl));
+    else
+      return badRequest("Diese Aufgabe existert leider nicht.");
+  }
+
+  public Result index() {
+    return ok(jsoverview.render(JsExercise.finder.all(), UserControl.getUser()));
+  }
+
   private List<JsTestResult> correct(JsExercise exercise, String learnerSolution) {
     try {
       ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-      
+
       // Lese programmierte Lernerlösung ein
       engine.eval(learnerSolution);
-      
+
       // Evaluiere Lernerlösung mit Testwerten
       List<JsTestResult> testResults = getTestResults(exercise.functionTests);
       for(JsTestResult testResult: testResults)
@@ -58,21 +70,9 @@ public class JS extends Controller {
       return Collections.emptyList();
     }
   }
-  
-  public Result exercise(int id) {
-    if(JsExercise.finder.byId(id) != null)
-      // TODO: Url for upload!
-      return ok(js.render(UserControl.getUser(), JsExercise.finder.byId(id), serverUrl));
-    else
-      return badRequest("Diese Aufgabe existert leider nicht.");
-  }
-  
+
   private List<JsTestResult> getTestResults(List<JsTest> functionTests) {
     return functionTests.stream().map(test -> new JsTestResult(test)).collect(Collectors.toList());
   }
-  
-  public Result index() {
-    return ok(jsoverview.render(UserControl.getUser(), JsExercise.finder.all()));
-  }
-  
+
 }
