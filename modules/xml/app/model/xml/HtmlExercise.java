161510aad8789d8aef9d5014d6b591b555f82345
s322414 <roman.kalweit@stud-mail.uni-wuerzburg.de>
2016-05-03 11:38:29 +0200

    copy of html

---
+++ modules/xml/app/model/xml/HtmlExercise.java
@@ -0,0 +1,36 @@
+package model.xml;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import model.html.task.Task;
+import play.data.validation.Constraints.Required;
+
+import com.avaje.ebean.Model;
+
+@Entity
+@Table(name = "htmlexercise")
+public class HtmlExercise extends Model {
+  
+  public static Finder<Integer, HtmlExercise> finder = new Finder<Integer, HtmlExercise>(HtmlExercise.class);
+  
+  @Id
+  public int id;
+  
+  @Required
+  public String title;
+  
+  @Required
+  @Column(name = "exerciseText", length = 1000)
+  public String exerciseText;
+  
+  @OneToMany(mappedBy = "exercise", cascade = CascadeType.ALL)
+  public List<Task> tasks;
+  
+}
