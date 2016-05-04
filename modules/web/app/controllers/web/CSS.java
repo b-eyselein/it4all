b814fcee61ddf7017b0eb388d9494e5f36c0808c
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-05-04 15:24:50 +0200

    Start CSS

--- modules/web/app/controllers/web/CSS.java
+++ modules/web/app/controllers/web/CSS.java
@@ -1,18 +1,42 @@
 package controllers.web;
 
 import java.util.Arrays;
+import java.util.List;
 
 import controllers.core.UserControl;
+import controllers.core.Util;
+import model.html.HtmlExercise;
+import model.html.result.ElementResult;
 import model.user.Secured;
+import model.user.User;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 import views.html.css.cssOverview;
+import views.html.css.css;
 
 @Security.Authenticated(Secured.class)
 public class CSS extends Controller {
+  
+  @Security.Authenticated(Secured.class)
+  public Result commit(int exerciseId) {
+    User user = UserControl.getUser();
 
+    if(request().accepts("application/json"))
+      return ok("{}");
+    else
+      // TODO: Definitive Abgabe Html, rendere Html!
+      return ok("TODO!");
+  }
+
+  public Result exercise(int id) {
+    
+    return ok(css.render(UserControl.getUser(), null, Util.getServerUrl()));
+  }
+  
   public Result index() {
     return ok(cssOverview.render(Arrays.asList("TEST...", "Test..."), UserControl.getUser()));
   }
+  
 }
