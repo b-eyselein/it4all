4f29d05c6e4565f157803bcb5697a1290762220e
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-05-09 13:31:00 +0200

    Definitive Abgabe von Html programmiert

--- public/lib/web/assets/processor.js
+++ public/lib/web/assets/processor.js
@@ -1,14 +1,29 @@
 function processCorrection(correction) {
   try {
     var newCorrection = JSON.parse(correction);
-    
+
+    var numOfSuccessfulResults = 0;
+
     for(i = 0; i < newCorrection.length; i++) {
       handleResult(newCorrection[i]);
+      if(newCorrection[i].success === "COMPLETE") {
+        numOfSuccessfulResults++;
+      }
+    }
+
+    var commitButton = document.getElementById("commit");
+    commitButton.disabled = false;
+    if(numOfSuccessfulResults == newCorrection.length) {
+      commitButton.title = "Sie k&ouml;nnen ihre L&ouml;sung abgeben.";
+      commitButton.className = "btn btn-success";
+    } else {
+      commitButton.title = "Sie k&ouml;nnen ihre L&ouml;sung abgeben, haben aber nicht alles richtig.";
+      commitButton.className = "btn btn-warning";
     }
   } catch(err) {
     alert(err);
   }
-  
+
   // TODO: Evtl. Update Live-Ansicht in andere Funktion auslagern
   // --> andere Aktivierung (onclick Data-toggle?)
   var live = document.getElementById("live");
@@ -18,7 +33,7 @@ function processCorrection(correction) {
 function handleResult(result) {
   var taskID = result.task.key.id;
   var taskDiv = document.getElementById("pan_task" + taskID);
-  
+
   if(result.success === "COMPLETE") {
     taskDiv.className = "panel panel-success";
     $("#task" + taskID).collapse('hide');
@@ -31,16 +46,16 @@ function handleResult(result) {
   } else {
     alert("Es gab einen Fehler!");
   }
-  
+
   var comDiv = document.getElementById("com_task" + taskID);
   comDiv.innerHTML = "";
-  
+
   if(result.success === "COMPLETE" || result.success === "PARTIALLY") {
     comDiv.innerHTML += "<div class=\"alert alert-success\">Element wurde gefunden!</div>";
   } else {
     comDiv.innerHTML += "<div class=\"alert alert-danger\">Element konnte nicht gefunden werden!</div>";
   }
-  
+
   for(attCount = 0; attCount < result.attributeResults.length; attCount++) {
     var attr = result.attributeResults[attCount];
     if(attr.success === "COMPLETE") {
@@ -57,7 +72,7 @@ function handleResult(result) {
           + attr.key + "\"</div>";
     }
   }
-  
+
   // TODO: childResults!
   for(childCount = 0; childCount < result.childResults.length; childCount++) {
     var childResult = result.childResults[childCount];
