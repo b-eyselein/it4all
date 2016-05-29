236baeafa82c20d1731f2e2bf617ba7f14965aad
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-05-29 11:33:10 +0200

    Test auskommentiert --> Wichtig: Implementieren!

--- modules/xml/test/CorrectorTest.java
+++ modules/xml/test/CorrectorTest.java
@@ -11,7 +11,7 @@ import model.CorrectorXml;
 import model.ElementResult;
 
 /**
- * 
+ *
  */
 
 /**
@@ -19,44 +19,45 @@ import model.ElementResult;
  *
  */
 public class CorrectorTest {
+  
+  /**
+   * Test method for
+   * {@link model.CorrectorXml#correctDTDAgainstXML(java.io.File)}.
+   */
+  @Test
+  public void testCorrectDTDAgainstXML() {
+    // FIXME: implement!
+    // fail("Not yet implemented");
+  }
 
-	/**
-	 * Test method for
-	 * {@link model.CorrectorXml#correctXMLAgainstDTD(java.io.File)}.
-	 */
-	@Test
-	public void testCorrectXMLAgainstDTD() {
-		File file = new File("test/resources/party.xml");
-		List<ElementResult> out = CorrectorXml.correctXMLAgainstDTD(file);
-		Assert.assertTrue(out.isEmpty());
-	}
+  /**
+   * Test method for
+   * {@link model.CorrectorXml#correctXMLAgainstDTD(java.io.File)}.
+   */
+  @Test
+  public void testCorrectXMLAgainstDTD() {
+    File file = new File("test/resources/party.xml");
+    List<ElementResult> out = CorrectorXml.correctXMLAgainstDTD(file);
+    Assert.assertTrue(out.isEmpty());
+  }
 
-	/**
-	 * Test method for
-	 * {@link model.CorrectorXml#correctXMLAgainstXSD(java.io.File, java.io.File)}
-	 * .
-	 */
-	@Test
-	public void testCorrectXMLAgainstXSD() {
-		File xml = new File("test/resources/note.xml");
-		File xsd = new File("test/resources/note.xsd");
-		List<ElementResult> out = null;
-		try {
-			out = CorrectorXml.correctXMLAgainstXSD(xsd, xml);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		Assert.assertNotNull(out);
-		Assert.assertTrue(out.isEmpty());
-	}
-
-	/**
-	 * Test method for
-	 * {@link model.CorrectorXml#correctDTDAgainstXML(java.io.File)}.
-	 */
-	@Test
-	public void testCorrectDTDAgainstXML() {
-		fail("Not yet implemented"); // TODO
-	}
+  /**
+   * Test method for
+   * {@link model.CorrectorXml#correctXMLAgainstXSD(java.io.File, java.io.File)}
+   * .
+   */
+  @Test
+  public void testCorrectXMLAgainstXSD() {
+    File xml = new File("test/resources/note.xml");
+    File xsd = new File("test/resources/note.xsd");
+    List<ElementResult> out = null;
+    try {
+      out = CorrectorXml.correctXMLAgainstXSD(xsd, xml);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    Assert.assertNotNull(out);
+    Assert.assertTrue(out.isEmpty());
+  }
 
 }
