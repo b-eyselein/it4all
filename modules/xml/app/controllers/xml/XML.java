3db2b4c518b0cdbe54d72c1621665841ef418748
rav <rav@posteo.net>
2016-05-31 17:23:44 +0200

    passt hoffentlich mit XMLError anstatt ElementResult

--- modules/xml/app/controllers/xml/XML.java
+++ modules/xml/app/controllers/xml/XML.java
@@ -11,10 +11,11 @@ import java.util.List;
 import java.util.ArrayList;
 
 import model.XmlExercise;
-import model.ElementResult;
-import model.Success;
-// import model.XmlCorrector;
-import model.CorrectorXml;
+import model.XMLError;
+import model.XmlCorrector;
+import model.XmlErrorType;
+import model.XMLError;
+import model.XmlCorrector;
 import model.user.Secured;
 import model.user.User;
 import play.Logger;
@@ -37,29 +38,29 @@ public class XML extends Controller {
   private static final String SERVER_URL = Util.getServerUrl();
   private static final String LEARNER_SOLUTION_VALUE = "editorContent";
   private static final String STANDARD_XML = "";
-
+  
   @Security.Authenticated(Secured.class)
   public Result commit(int exerciseId) {
     User user = UserControl.getCurrentUser();
-
+    
     String learnerSolution = extractLearnerSolutionFromRequest(request());
     Logger.info(learnerSolution);
     Path path2solution = saveSolutionForUser(user.name, learnerSolution, exerciseId);
     Logger.info(path2solution.toString());
-
-    List<ElementResult> elementResults = correctExercise(path2solution, user, XmlExercise.finder.byId(exerciseId));
-
+    
+    List<XMLError> elementResults = correctExercise(path2solution, user, XmlExercise.finder.byId(exerciseId));
+    
     if(request().acceptedTypes().get(0).toString().equals("application/json"))
       // print this JSON-tree!!! to know what is inside
       return ok(Json.toJson(elementResults));
     else
       return ok(xmlcorrect.render(learnerSolution, elementResults, UserControl.getCurrentUser()));
   }
-
+  
   @Security.Authenticated(Secured.class)
   public Result exercise(int exerciseId) {
     XmlExercise exercise = XmlExercise.finder.byId(exerciseId);
-
+    
     if(exercise == null)
       return badRequest(new Html("<p>Diese Aufgabe existert leider nicht.</p><p>Zur&uuml;ck zur <a href=\""
           + routes.XML.index() + "\">Startseite</a>.</p>"));
@@ -74,7 +75,7 @@ public class XML extends Controller {
     } catch (IOException e) {
       Logger.error(e.getMessage());
     }
-
+    
     String referenceCode = "";
     try {
       Path referenceFilePath = Util.getXmlReferenceFilePath(exercise.referenceFileName);
@@ -84,45 +85,53 @@ public class XML extends Controller {
     } catch (IOException e) {
       Logger.error(e.getMessage());
     }
-
+    
     return ok(xml.render(UserControl.getCurrentUser(), exercise, referenceCode, defaultOrOldSolution, SERVER_URL));
   }
-
+  
   @Security.Authenticated(Secured.class)
   public Result index() {
     return ok(xmloverview.render(XmlExercise.finder.all(), UserControl.getCurrentUser()));
   }
-
+  
   /**
    * Replaces characters which cause problems when displayed in html. private
    * String escapeCode(String code) { return code.replaceAll("<",
    * "&lt;").replaceAll(">", "&gt;").replaceAll("&", "&amp;"); }
    */
-
-  private List<ElementResult> correctExercise(Path solutionPath, User user, XmlExercise exercise) {
-	  File solutionFile = new File(solutionPath.toString());
-	  File referenceFile = new File(Util.getXmlReferenceFilePath(exercise.referenceFileName).toString());
-	  List<ElementResult> result = CorrectorXml.correct(solutionFile, referenceFile, exercise, user);
-//    List<ElementResult> result = new ArrayList<ElementResult>();
-//    result.add(new ElementResult(Success.PARTIALLY, "Test Result", "message"));
-      if (result.isEmpty()) {
-	    result.add(new ElementResult(Success.COMPLETE, "Super!", "Bist ein ganz guter Student."));
-	  }
-	  boolean malformed = false;
-	  for (ElementResult el : result) {
-	    if (el.getSuccess() == Success.NONE)
-		   malformed = true;
-	  }
-	  if (!result.isEmpty() && !malformed) {
-	    result.add(new ElementResult(Success.COMPLETE, "Die Eingabe ist wohlgeformt", ""));
-	  }
+  
+  private List<XMLError> correctExercise(Path solutionPath, User user, XmlExercise exercise) {
+    File solutionFile = new File(solutionPath.toString());
+    File referenceFile = new File(Util.getXmlReferenceFilePath(exercise.referenceFileName).toString());
+    List<XMLError> result = null;
+    try {
+      result = XmlCorrector.correct(solutionFile, referenceFile, exercise, user);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    // List<ElementResult> result = new ArrayList<ElementResult>();
+    // result.add(new ElementResult(Success.PARTIALLY, "Test Result",
+    // "message"));
+    if(result.isEmpty()) {
+      result.add(new XMLError(XmlErrorType.NONE, "Super!", "Bist ein ganz guter Student."));
+    }
+    boolean malformed = false;
+    for(XMLError el: result) {
+      // TODO: passt hier pruefen auf FATALERROR oder vielleicht doch lieber "el.getErrorType() != XmlErrorType.NONE"
+      if(el.getErrorType() == XmlErrorType.FATALERROR)
+        malformed = true;
+    }
+    if(!result.isEmpty() && !malformed) {
+      result.add(new XMLError(XmlErrorType.NONE, "Die Eingabe ist wohlgeformt", ""));
+    }
     return result;
   }
-
+  
   private String extractLearnerSolutionFromRequest(Request request) {
     return request.body().asFormUrlEncoded().get(LEARNER_SOLUTION_VALUE)[0];
   }
-
+  
   private Path saveSolutionForUser(String userName, String solution, int exercise) {
     try {
       if(!Files.exists(Util.getSolDirForUser(userName)))
@@ -140,5 +149,5 @@ public class XML extends Controller {
     }
     return null;
   }
-
+  
 }
