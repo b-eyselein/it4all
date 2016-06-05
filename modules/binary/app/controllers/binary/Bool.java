16d3d1d4313251a9b35da8024b0be14a86e659c7
johannes <joh.hefter@googlemail.com>
2016-06-05 19:49:25 +0200

    auto generated tables

--- modules/binary/app/controllers/binary/Bool.java
+++ modules/binary/app/controllers/binary/Bool.java
@@ -2,36 +2,49 @@ package controllers.binary;
 
 import javax.inject.Inject;
 
+import controllers.core.UserManagement;
 import model.Secured;
 import play.data.DynamicForm;
 import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
-import views.html.TableThree;
+import views.html.boolquestion;
+import model.boolescheAlgebra.*;
+import model.boolescheAlgebra.BFTree.*;
 
 @Security.Authenticated(Secured.class)
 public class Bool extends Controller {
   
   @Inject
   FormFactory factory;
-
+  BoolescheFunktionTree bft;
+  BoolescheFunktionenGenerator bGen = new BoolescheFunktionenGenerator();
   String[] solutions;
+  int length;
 
   public Result index() {
-    return ok(TableThree.render("a", "b", "c", "a & b & c"));
+    bft = bGen.neueBoolescheFunktion();
+    double d = (double) bft.getAnzahlVariablen();
+    length = (int)(Math.pow(2.0, d));
+    return ok(boolquestion.render(bft.toString(),bft.getVariablen(), UserManagement.getCurrentUser(), bft.getVariablenTabelle(), length));
   }
 
-  public Result indexThree() {
-    return ok(" " + solutions[0] + " " + solutions[7]);
-  }
-
-  public Result tableThreeAdd() {
-    solutions = new String[8];
-    DynamicForm dynFormula = factory.form().bindFromRequest();
-    for(int i = 1; i < 9; i++) {
-      solutions[(i - 1)] = dynFormula.get("sol" + i + "");
+  public Result indexSolution() {
+    String s = "";
+    for(int i = 0; i<length; i++)
+    {
+      s = s+solutions[i];
     }
-    return redirect(routes.Bool.indexThree());
+    return ok(s);
+  }
+
+  public Result tableAdd() {
+    solutions = new String[length];
+    DynamicForm dynFormula = factory.form().bindFromRequest();
+    for(int i = 0; i < length; i++) {
+      solutions[i] = dynFormula.get("" + i + "");
+    }
+    return redirect(routes.Bool.indexSolution());
   }
 }
