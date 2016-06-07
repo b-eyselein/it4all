64848edfcd91af8e85f6d3a58d74ea766a4a3e20
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-06-07 13:28:40 +0200

    Aktualisierung Binärtest

--- modules/binary/test/model/NAryTests/Test_Constructors.java
+++ modules/binary/test/model/NAryTests/Test_Constructors.java
@@ -1,6 +1,7 @@
 package model.NAryTests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import model.NAryNumbers.NAryNumber;
@@ -10,31 +11,33 @@ public class Test_Constructors {
   @Test
   public void testNegatives() {
     NAryNumber hex_234 = new NAryNumber(-234, 16);
-    NAryNumber bin_234 = new NAryNumber(-234, 2);
-    System.out.println(hex_234.toString());
-  }
+    assertEquals(hex_234.toString(), "-EA");
 
+    NAryNumber bin_234 = new NAryNumber(-234, 2);
+    assertEquals(bin_234.toString(), "-11101010");
+  }
+  
   @Test
   public void testPositives() {
     NAryNumber hex_234 = new NAryNumber(234, 16);
-    NAryNumber oct_234 = new NAryNumber(234, 8);
-    NAryNumber bin_234 = new NAryNumber(234, 2);
-    NAryNumber bin_127 = new NAryNumber(127, 2);
     NAryNumber oct_63 = new NAryNumber(63, 8);
-
+    
     // Testing for the right value
     assertEquals(hex_234.getValue(), 234);
     assertEquals(oct_63.getValue(), 63);
-
+    
     // Testing for the right base
     assertEquals(hex_234.getBase(), 16);
     assertEquals(oct_63.getBase(), 8);
-
+    
     // Testing for the right representation
+    NAryNumber bin_127 = new NAryNumber(127, 2);
+    NAryNumber bin_234 = new NAryNumber(234, 2);
+    NAryNumber oct_234 = new NAryNumber(234, 8);
     assertEquals(hex_234.toString(), "EA");
-    assertEquals(bin_234.toString(), "11101010");
     assertEquals(oct_234.toString(), "352");
+    assertEquals(bin_234.toString(), "11101010");
     assertEquals(bin_127.toString(), "1111111");
   }
-
+  
 }
