13606d135eb687ad527a7bc860a36e1657cbe50a
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-06-13 15:45:13 +0200

    Weitere Implementierung Erstellung Aufgaben XML mittels Json

--- modules/xml/app/controllers/xml/XmlStartUpChecker.java
+++ modules/xml/app/controllers/xml/XmlStartUpChecker.java
@@ -12,6 +12,7 @@ import javax.inject.Inject;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import model.ExerciseType;
 import model.Util;
 import model.XmlExercise;
 import play.Logger;
@@ -20,17 +21,17 @@ import play.libs.Json;
 public class XmlStartUpChecker {
   
   private static Logger.ALogger theLogger = Logger.of("startup");
-  
+
   private Util util;
-  
+
   @Inject
   public XmlStartUpChecker(Util theUtil) {
     util = theUtil;
     performStartUpCheck();
-    
+
     testJsonCreation();
   }
-  
+
   public void performStartUpCheck() {
     List<XmlExercise> exercises = XmlExercise.finder.all();
     if(exercises.size() == 0)
@@ -39,7 +40,7 @@ public class XmlStartUpChecker {
       for(XmlExercise exercise: exercises)
         checkOrCreateSampleFile(exercise);
   }
-  
+
   private void checkOrCreateSampleFile(XmlExercise exercise) {
     Path sampleFile = util.getSampleFileForExercise("xml", exercise.referenceFileName);
     if(Files.exists(sampleFile))
@@ -47,7 +48,7 @@ public class XmlStartUpChecker {
     
     theLogger.warn("Die Lösungsdatei für Xml-Aufgabe " + exercise.id + " \"" + sampleFile
         + "\" existiert nicht! Versuche, Datei zu erstellen...");
-    
+
     Path providedFile = Paths.get("modules/xml/conf/resources", exercise.referenceFileName);
     if(Files.exists(providedFile))
       try {
@@ -59,7 +60,7 @@ public class XmlStartUpChecker {
     else
       theLogger.error("Konnte Datei nicht erstellen: Keine Lösungsdatei mitgeliefert...");
   }
-  
+
   private void testJsonCreation() {
     // FIXME: Generate/Read exercises from this file
     String fileName = "modules/xml/conf/resources/exercises.json";
@@ -67,11 +68,22 @@ public class XmlStartUpChecker {
       String jsonAsString = String.join("\n", Files.readAllLines(Paths.get(fileName)));
       JsonNode json = Json.parse(jsonAsString);
       Iterator<JsonNode> childNodes = json.elements();
-      while(childNodes.hasNext())
-        theLogger.debug(Json.prettyPrint(childNodes.next()));
+      while(childNodes.hasNext()) {
+        JsonNode node = childNodes.next();
+        XmlExercise newExercise = new XmlExercise();
+        newExercise.id = node.get("id").asInt();
+        newExercise.title = node.get("title").asText();
+        // TODO: exerciseType!
+        newExercise.exerciseType = ExerciseType.valueOf(node.get("exerciseType").asText());
+        newExercise.referenceFileName = node.get("referenceFileName").asText();
+        newExercise.exerciseText = node.get("exerciseText").asText();
+
+        theLogger.debug(newExercise.id + ":\n\t" + newExercise.title + "\n\t" + newExercise.exerciseText + "\n\t"
+            + newExercise.referenceFileName + "\n\t" + newExercise.exerciseType);
+      }
     } catch (IOException e) {
       theLogger.error("Fehler beim Lesen aus der Datei " + fileName, e);
     }
   }
-  
+
 }
