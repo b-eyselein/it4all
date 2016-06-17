40c49715b9c7d19971b0171f2dce2d9fd6b23758
rav <rav@posteo.net>
2016-06-17 15:18:01 +0200

    XMLError mit type als Titel

--- modules/xml/app/model/SimpleXMLErrorHandler.java
+++ modules/xml/app/model/SimpleXMLErrorHandler.java
@@ -11,36 +11,36 @@ import java.util.List;
 
 public class SimpleXMLErrorHandler implements ErrorHandler {
   private List<XMLError> Output;
-  
+
   public SimpleXMLErrorHandler(List<XMLError> output) {
     this.Output = output;
   }
-  
+
   @Override
   public void warning(SAXParseException exception) throws SAXException {
     if(exception.getLineNumber() > -1) {
       Output.add(new XMLError(exception.getLineNumber(), exception.getMessage(), XmlErrorType.WARNING));
     } else {
-      Output.add(new XMLError("", exception.getMessage(), XmlErrorType.WARNING));
+      Output.add(new XMLError(XmlErrorType.WARNING.toString(), exception.getMessage(), XmlErrorType.WARNING));
     }
   }
-  
+
   @Override
   public void fatalError(SAXParseException exception) throws SAXException {
     if(exception.getLineNumber() > -1) {
       Output.add(new XMLError(exception.getLineNumber(), exception.getMessage(), XmlErrorType.FATALERROR));
     } else {
-      Output.add(new XMLError("", exception.getMessage(), XmlErrorType.FATALERROR));
+      Output.add(new XMLError(XmlErrorType.FATALERROR.toString(), exception.getMessage(), XmlErrorType.FATALERROR));
     }
   }
-  
+
   @Override
   public void error(SAXParseException exception) throws SAXException {
     if(exception.getLineNumber() > -1) {
       Output.add(new XMLError(exception.getLineNumber(), exception.getMessage(), XmlErrorType.ERROR));
     } else {
-      Output.add(new XMLError("", exception.getMessage(), XmlErrorType.ERROR));
+      Output.add(new XMLError(XmlErrorType.ERROR.toString(), exception.getMessage(), XmlErrorType.ERROR));
     }
-    
+
   }
 }
