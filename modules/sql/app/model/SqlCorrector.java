e5d278b50c95416b6334cc3f43ff971afbea2473
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-06-21 15:13:23 +0200

    Weitere Implementierung Vergleich SQL-Resultate

--- modules/sql/app/model/SqlCorrector.java
+++ modules/sql/app/model/SqlCorrector.java
@@ -7,13 +7,13 @@ public class SqlCorrector {
   
   public static boolean correct(String statement, SqlExercise exercise, Connection conn) {
     try {
-      SqlQueryResult userResult = new SqlQueryResult(conn.createStatement().executeQuery(statement));
-      SqlQueryResult sampleResult = new SqlQueryResult(conn.createStatement().executeQuery(exercise.sample));
-
+      SqlQueryResult userResult = new SqlQueryResult(conn.createStatement().executeQuery(statement), true);
+      SqlQueryResult sampleResult = new SqlQueryResult(conn.createStatement().executeQuery(exercise.sample), true);
+      
       return userResult.isIdentic(sampleResult);
     } catch (SQLException e) {
       return false;
     }
   }
-
+  
 }
--- modules/sql/app/model/SqlQueryResult.java
+++ modules/sql/app/model/SqlQueryResult.java
@@ -4,53 +4,69 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-import play.Logger;
-
 public class SqlQueryResult {
   
-  private static final Logger.ALogger theLogger = Logger.of("sql");
-
   private List<String> colNames;
   private List<List<String>> rows;
-
-  public SqlQueryResult(ResultSet resultSet) {
-    try {
-      ResultSetMetaData metaData = resultSet.getMetaData();
-      int columnCount = metaData.getColumnCount();
-
-      colNames = new ArrayList<String>(columnCount);
-      for(int i = 1; i <= columnCount; i++)
-        colNames.add(metaData.getColumnName(i));
-      
-      rows = new LinkedList<List<String>>();
-      while(resultSet.next()) {
-        List<String> row = new ArrayList<String>(columnCount);
-        for(int i = 1; i <= columnCount; i++)
-          row.add(resultSet.getString(i));
-        rows.add(row);
-      }
-
-    } catch (SQLException e) {
-      theLogger.error("Fehler bei Verarbeitung von ResultSet " + resultSet, e);
-    }
+  
+  public SqlQueryResult(ResultSet resultSet) throws SQLException {
+    this(resultSet, false);
   }
-
+  
+  public SqlQueryResult(ResultSet resultSet, boolean sort) throws SQLException {
+    ResultSetMetaData metaData = resultSet.getMetaData();
+    int columnCount = metaData.getColumnCount();
+    
+    colNames = new ArrayList<String>(columnCount);
+    for(int i = 1; i <= columnCount; i++)
+      colNames.add(metaData.getColumnName(i));
+    
+    if(sort)
+      // Sort column names to ignore order
+      Collections.sort(colNames);
+    
+    rows = new LinkedList<List<String>>();
+    while(resultSet.next()) {
+      List<String> row = new ArrayList<String>(columnCount);
+      for(int i = 1; i <= columnCount; i++)
+        row.add(resultSet.getString(colNames.get(i - 1)));
+      rows.add(row);
+    }
+    
+  }
+  
+  public int getColumnCount() {
+    return colNames.size();
+  }
+  
   public List<String> getColumnNames() {
     return colNames;
   }
-
+  
   public List<List<String>> getRows() {
     return rows;
   }
-
+  
   public boolean isIdentic(SqlQueryResult other) {
     // FIXME: implement comparison!
-    if(other.getColumnNames().size() != colNames.size())
+    
+    // Check columnNames with order
+    int columnCount = colNames.size();
+    if(other.getColumnCount() != columnCount)
+      return false;
+    for(int i = 0; i < columnCount; i++)
+      if(!other.getColumnNames().get(i).equals(colNames.get(i)))
+        return false;
+    
+    // Check rows
+    int rowCount = rows.size();
+    if(other.getRows().size() != rowCount)
       return false;
     return true;
   }
-
+  
 }
