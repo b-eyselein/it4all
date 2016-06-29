4c465c18f8316b1786446b2024dfecbfe115f627
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-06-29 13:15:13 +0200

    Überarbeitung Unittests

--- modules/binary/test/model/NAryTests/Test_Constructors.java
+++ modules/binary/test/model/NAryTests/Test_Constructors.java
@@ -5,35 +5,36 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import model.NAryNumbers.NAryNumber;
+import model.NAryNumbers.questions.NumberBase;
 
 public class Test_Constructors {
   
   @Test
   public void testNegatives() {
-    NAryNumber hex_234 = new NAryNumber(-234, 16);
+    NAryNumber hex_234 = new NAryNumber(-234, NumberBase.HEXADECIMAL);
     assertEquals(hex_234.toString(), "-EA");
-
-    NAryNumber bin_234 = new NAryNumber(-234, 2);
+    
+    NAryNumber bin_234 = new NAryNumber(-234, NumberBase.BINARY);
     assertEquals(bin_234.toString(), "-11101010");
   }
   
   @Test
   public void testPositives() {
-    NAryNumber hex_234 = new NAryNumber(234, 16);
-    NAryNumber oct_63 = new NAryNumber(63, 8);
+    NAryNumber hex_234 = new NAryNumber(234, NumberBase.HEXADECIMAL);
+    NAryNumber oct_63 = new NAryNumber(63, NumberBase.OCTAL);
     
     // Testing for the right value
     assertEquals(hex_234.getValue(), 234);
     assertEquals(oct_63.getValue(), 63);
     
     // Testing for the right base
-    assertEquals(hex_234.getBase(), 16);
-    assertEquals(oct_63.getBase(), 8);
+    assertEquals(hex_234.getBase(), NumberBase.HEXADECIMAL);
+    assertEquals(oct_63.getBase(), NumberBase.OCTAL);
     
     // Testing for the right representation
-    NAryNumber bin_127 = new NAryNumber(127, 2);
-    NAryNumber bin_234 = new NAryNumber(234, 2);
-    NAryNumber oct_234 = new NAryNumber(234, 8);
+    NAryNumber bin_127 = new NAryNumber(127, NumberBase.BINARY);
+    NAryNumber bin_234 = new NAryNumber(234, NumberBase.BINARY);
+    NAryNumber oct_234 = new NAryNumber(234, NumberBase.OCTAL);
     assertEquals(hex_234.toString(), "EA");
     assertEquals(oct_234.toString(), "352");
     assertEquals(bin_234.toString(), "11101010");
--- modules/binary/test/model/NAryTests/Test_StringToNaAry.java
+++ modules/binary/test/model/NAryTests/Test_StringToNaAry.java
@@ -4,67 +4,63 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import model.NAryNumbers.NAryNumber;
+import model.NAryNumbers.questions.NumberBase;
 
-public class Test_StringToNaAry
-{
+public class Test_StringToNaAry {
 
-	@Test
-	public void testPositive()
-	{
-		String str_hex_234_1 = "ea";
-		String str_hex_234_2 = "EA";
-		String str_hex_234_3 = "0xEA";
-		String str_hex_234_4 = "0XeA";
-		
-		NAryNumber hex_1 = NAryNumber.stringToNAry(str_hex_234_1,16);
-		NAryNumber hex_2 = NAryNumber.stringToNAry(str_hex_234_2,16);
-		NAryNumber hex_3 = NAryNumber.stringToNAry(str_hex_234_3,16);
-		NAryNumber hex_4 = NAryNumber.stringToNAry(str_hex_234_4,16);
-		NAryNumber hex_5 = NAryNumber.StringToNAry(str_hex_234_3);
-		NAryNumber hex_6 = NAryNumber.StringToNAry(str_hex_234_4);
-		
-		assertEquals(hex_1.toString(),str_hex_234_2);
-		assertEquals(hex_5.toString(),str_hex_234_2);
-		assertEquals(hex_6.toString(),str_hex_234_2);
-		assertEquals(hex_6.getValue(),234);
-		assertTrue(hex_3.equals(hex_4));
-		assertTrue(hex_2.equals(hex_6));
-	}
-	
-	@Test
-	public void testNegative()
-	{
-		String str_hex_234_1 = "-ea";
-		String str_hex_234_2 = "-EA";
-		String str_hex_234_3 = "-0xEA";
-		String str_hex_234_4 = "-0XeA";
-		
-		NAryNumber hex_1 = NAryNumber.stringToNAry(str_hex_234_1,16);
-		NAryNumber hex_2 = NAryNumber.stringToNAry(str_hex_234_2,16);
-		NAryNumber hex_3 = NAryNumber.stringToNAry(str_hex_234_3,16);
-		NAryNumber hex_4 = NAryNumber.stringToNAry(str_hex_234_4,16);
-		NAryNumber hex_5 = NAryNumber.StringToNAry(str_hex_234_3);
-		NAryNumber hex_6 = NAryNumber.StringToNAry(str_hex_234_4);
-		
-		assertEquals(hex_1.toString(),str_hex_234_2);
-		assertEquals(hex_5.toString(),str_hex_234_2);
-		assertEquals(hex_6.toString(),str_hex_234_2);
-		assertEquals(hex_6.getValue(),-234);
-		assertTrue(hex_3.equals(hex_4));
-		assertTrue(hex_2.equals(hex_6));
-	}
-	
-	@Test (expected = IllegalArgumentException.class)
-	public void testWrongCharacter()
-	{
-		String str_hex_234_1 = "eaZ";
-		NAryNumber hex_1 = NAryNumber.stringToNAry(str_hex_234_1,16);
-	}
-	
-	@Test (expected = IllegalArgumentException.class)
-	public void testMissingPrefix()
-	{
-		String str_hex_234_1 = "ea";
-		NAryNumber hex_1 = NAryNumber.StringToNAry(str_hex_234_1);
-	}
+  @Test(expected = IllegalArgumentException.class)
+  public void testMissingPrefix() {
+    String str_hex_234_1 = "ea";
+    NAryNumber.StringToNAry(str_hex_234_1);
+  }
+
+  @Test
+  public void testNegative() {
+    String str_hex_234_1 = "-ea";
+    String str_hex_234_2 = "-EA";
+    String str_hex_234_3 = "-0xEA";
+    String str_hex_234_4 = "-0XeA";
+
+    NAryNumber hex_1 = NAryNumber.stringToNAry(str_hex_234_1, NumberBase.HEXADECIMAL);
+    NAryNumber hex_2 = NAryNumber.stringToNAry(str_hex_234_2, NumberBase.HEXADECIMAL);
+    NAryNumber hex_3 = NAryNumber.stringToNAry(str_hex_234_3, NumberBase.HEXADECIMAL);
+    NAryNumber hex_4 = NAryNumber.stringToNAry(str_hex_234_4, NumberBase.HEXADECIMAL);
+    NAryNumber hex_5 = NAryNumber.StringToNAry(str_hex_234_3);
+    NAryNumber hex_6 = NAryNumber.StringToNAry(str_hex_234_4);
+
+    assertEquals(hex_1.toString(), str_hex_234_2);
+    assertEquals(hex_5.toString(), str_hex_234_2);
+    assertEquals(hex_6.toString(), str_hex_234_2);
+    assertEquals(hex_6.getValue(), -234);
+    assertTrue(hex_3.equals(hex_4));
+    assertTrue(hex_2.equals(hex_6));
+  }
+
+  @Test
+  public void testPositive() {
+    String str_hex_234_1 = "ea";
+    String str_hex_234_2 = "EA";
+    String str_hex_234_3 = "0xEA";
+    String str_hex_234_4 = "0XeA";
+
+    NAryNumber hex_1 = NAryNumber.stringToNAry(str_hex_234_1, NumberBase.HEXADECIMAL);
+    NAryNumber hex_2 = NAryNumber.stringToNAry(str_hex_234_2, NumberBase.HEXADECIMAL);
+    NAryNumber hex_3 = NAryNumber.stringToNAry(str_hex_234_3, NumberBase.HEXADECIMAL);
+    NAryNumber hex_4 = NAryNumber.stringToNAry(str_hex_234_4, NumberBase.HEXADECIMAL);
+    NAryNumber hex_5 = NAryNumber.StringToNAry(str_hex_234_3);
+    NAryNumber hex_6 = NAryNumber.StringToNAry(str_hex_234_4);
+
+    assertEquals(hex_1.toString(), str_hex_234_2);
+    assertEquals(hex_5.toString(), str_hex_234_2);
+    assertEquals(hex_6.toString(), str_hex_234_2);
+    assertEquals(hex_6.getValue(), 234);
+    assertTrue(hex_3.equals(hex_4));
+    assertTrue(hex_2.equals(hex_6));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testWrongCharacter() {
+    String str_hex_234_1 = "eaZ";
+    NAryNumber.stringToNAry(str_hex_234_1, NumberBase.HEXADECIMAL);
+  }
 }
