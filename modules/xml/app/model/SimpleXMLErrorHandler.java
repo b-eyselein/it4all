c15fb6c9f7aad3e96472ad81ea382776e3a35b29
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-06-29 13:15:00 +0200

    Organizing imports

--- modules/xml/app/model/SimpleXMLErrorHandler.java
+++ modules/xml/app/model/SimpleXMLErrorHandler.java
@@ -6,42 +6,38 @@ import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
-import org.xml.sax.ErrorHandler;
-
-import java.util.List;
-
 public class SimpleXMLErrorHandler implements ErrorHandler {
-  private List<XMLError> Output;
-  
+  private List<XMLError> output;
+
   public SimpleXMLErrorHandler(List<XMLError> output) {
-    this.Output = output;
+    this.output = output;
   }
-  
+
   @Override
   public void error(SAXParseException exception) throws SAXException {
     if(exception.getLineNumber() > -1) {
-      Output.add(new XMLError(exception.getLineNumber(), exception.getMessage(), XmlErrorType.ERROR));
+      output.add(new XMLError(exception.getLineNumber(), exception.getMessage(), XmlErrorType.ERROR));
     } else {
-      Output.add(new XMLError(XmlErrorType.ERROR.toString(), exception.getMessage(), XmlErrorType.ERROR));
+      output.add(new XMLError(XmlErrorType.ERROR.toString(), exception.getMessage(), XmlErrorType.ERROR));
     }
-    
+
   }
-  
+
   @Override
   public void fatalError(SAXParseException exception) throws SAXException {
     if(exception.getLineNumber() > -1) {
-      Output.add(new XMLError(exception.getLineNumber(), exception.getMessage(), XmlErrorType.FATALERROR));
+      output.add(new XMLError(exception.getLineNumber(), exception.getMessage(), XmlErrorType.FATALERROR));
     } else {
-      Output.add(new XMLError(XmlErrorType.FATALERROR.toString(), exception.getMessage(), XmlErrorType.FATALERROR));
+      output.add(new XMLError(XmlErrorType.FATALERROR.toString(), exception.getMessage(), XmlErrorType.FATALERROR));
     }
   }
-  
+
   @Override
   public void warning(SAXParseException exception) throws SAXException {
     if(exception.getLineNumber() > -1) {
-      Output.add(new XMLError(exception.getLineNumber(), exception.getMessage(), XmlErrorType.WARNING));
+      output.add(new XMLError(exception.getLineNumber(), exception.getMessage(), XmlErrorType.WARNING));
     } else {
-      Output.add(new XMLError(XmlErrorType.WARNING.toString(), exception.getMessage(), XmlErrorType.WARNING));
+      output.add(new XMLError(XmlErrorType.WARNING.toString(), exception.getMessage(), XmlErrorType.WARNING));
     }
 
   }
