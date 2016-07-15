bca02cf8b198414261cf9cf7b0b5c54a7755d26e
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-07-15 14:44:49 +0200

    Kleine Änderungen

--- modules/binary/test/model/boolescheAlgebra/BFTree/AssignmentTest.java
+++ modules/binary/test/model/boolescheAlgebra/BFTree/AssignmentTest.java
@@ -1,5 +1,7 @@
 package model.boolescheAlgebra.BFTree;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.util.List;
 
 import org.junit.Test;
@@ -8,25 +10,28 @@ public class AssignmentTest {
   
   @Test
   public void testGenerateAllAssignments() {
+    // TODO!
     char[] variables = {'a', 'b', 'c'};
     List<Assignment> assignments = Assignment.generateAllAssignments(variables);
-    for(Assignment a: assignments)
-      System.out.println(a);
+    assertNotNull(assignments);
     // fail("Not yet implemented");
   }
   
   @Test
   public void testGetAssignment() {
+    // TODO!
     // fail("Not yet implemented");
   }
   
   @Test
   public void testSetAssignment() {
+    // TODO!
     // fail("Not yet implemented");
   }
   
   @Test
   public void testToString() {
+    // TODO!
     // fail("Not yet implemented");
   }
   
