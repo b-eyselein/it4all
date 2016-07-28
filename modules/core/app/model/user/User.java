b9ce31028ce765bc9abc03f43c11a7148697ea01
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-07-28 14:06:54 +0200

    Small Formatting User

--- modules/core/app/model/user/User.java
+++ modules/core/app/model/user/User.java
@@ -5,10 +5,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-//import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-//import model.html.Grading;
 
 import com.avaje.ebean.Model;
 
@@ -18,15 +15,15 @@ import play.mvc.PathBindable;
 @Entity
 @Table(name = "users")
 public class User extends Model implements PathBindable<User> {
-  
-  public static Finder<String, User> finder = new Finder<String, User>(User.class);
-  
+
+  public static Finder<String, User> finder = new Finder<>(User.class);
+
   @OneToMany(mappedBy = "user")
   public List<Grading> gradings;
-  
+
   @Id
   public String name;
-  
+
   @Override
   public User bind(String key, String name) {
     User user = finder.byId(name);
@@ -34,15 +31,15 @@ public class User extends Model implements PathBindable<User> {
       throw new IllegalArgumentException("User with name " + name + " not found!");
     return user;
   }
-  
+
   @Override
   public String javascriptUnbind() {
     return "function(k, v) {\n    return v.name;\n}";
   }
-  
+
   @Override
   public String unbind(String key) {
     return name;
   }
-  
+
 }
