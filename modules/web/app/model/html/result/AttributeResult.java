cc76ea23d0b214848efb3fbfab201b367816e8ea
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-08-23 13:27:19 +0200

    Fix für nicht angegebene Attribute bei HTML

--- modules/web/app/model/html/result/AttributeResult.java
+++ modules/web/app/model/html/result/AttributeResult.java
@@ -4,20 +4,28 @@ import org.openqa.selenium.WebElement;
 
 import model.exercise.EvaluationResult;
 import model.exercise.Success;
+import play.Logger;
 
 public class AttributeResult extends EvaluationResult {
-  
+
   private String key;
   private String value;
-  
+
   public AttributeResult(String attributeKey, String attributeValue) {
     super(Success.NONE);
     key = attributeKey;
     value = attributeValue;
   }
-  
+
   public void evaluate(WebElement element) {
-    String foundValue = element.getAttribute(key);
+    String foundValue = null;
+    try {
+      foundValue = element.getAttribute(key);
+    } catch (NoSuchMethodError e) {
+      // TODO: Better solution! Catch NoSuchMethodError if attribute is not
+      // declared
+      Logger.error("Error while searching for attribute: ", e);
+    }
     if(foundValue == null)
       success = Success.NONE;
     else if(foundValue.equals(value))
@@ -25,13 +33,13 @@ public class AttributeResult extends EvaluationResult {
     else
       success = Success.PARTIALLY;
   }
-  
+
   public String getKey() {
     return key;
   }
-  
+
   public String getValue() {
     return value;
   }
-  
+
 }
