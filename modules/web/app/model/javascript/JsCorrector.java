ea3367e86170f1754bca44240a98e82e2baa0d03
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-08-23 16:41:37 +0200

    FIXMEs

--- modules/web/app/model/javascript/JsCorrector.java
+++ modules/web/app/model/javascript/JsCorrector.java
@@ -1,6 +1,6 @@
 package model.javascript;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,36 +11,41 @@ import javax.script.ScriptException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+import model.exercise.Success;
 import model.javascript.web.JsWebExercise;
 import model.javascript.web.JsWebTestResult;
 import play.Logger;
 
 public class JsCorrector {
-  
+
   public static List<JsTestResult> correct(JsExercise exercise, String learnerSolution) {
     ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
     try {
       // Lese programmierte Lernerlösung ein
       engine.eval(learnerSolution);
     } catch (ScriptException e) {
+      // FIXME javax.script.ScriptException an Lerner! --> Syntaxfehler!
+      // FIXME: Testdaten des Nutzers!
       // TODO: Log Exception or submit to learner?
       Logger.error("Fehler beim Laden der Lernerlösung", e);
-      return Collections.emptyList();
+      return Arrays.asList(new JsTestResult(exercise.functionTests.get(0), Success.NONE, "", ""));
     }
-    
+
+    // FIXME: Es gab einen Fehler beim Ausführen des Codes. Woher??
+
     // Evaluiere Lernerlösung mit Testwerten
     return exercise.functionTests.stream().map(test -> test.evaluate(engine)).collect(Collectors.toList());
-    
+
   }
-  
+
   public static List<JsWebTestResult> correctWeb(JsWebExercise exercise, String solutionUrl) {
     WebDriver driver = loadWebSite(solutionUrl);
-    
+
     List<JsWebTestResult> results = exercise.tests.stream().map(test -> test.test(driver)).collect(Collectors.toList());
-    
+
     return results;
   }
-  
+
   private static WebDriver loadWebSite(String solutionUrl) {
     WebDriver driver = new HtmlUnitDriver(true);
     driver.get(solutionUrl);
