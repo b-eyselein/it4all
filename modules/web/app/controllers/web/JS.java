b7ebc75f320e7da7d74580bef24b1eae0fcca605
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-08-26 15:03:36 +0200

    Kommentiert

--- modules/web/app/controllers/web/JS.java
+++ modules/web/app/controllers/web/JS.java
@@ -36,79 +36,81 @@ import views.html.javascript.jsweb;
 
 @Security.Authenticated(Secured.class)
 public class JS extends Controller {
-  
+
   private static final String EXERCISE_TYPE = "js";
   private static final String FILE_TYPE = "html";
-  
+
   @Inject
   private Util util;
-  
+
   @Inject
   private FormFactory factory;
-  
+
   public Result commit(int exerciseId) {
     User user = UserManagement.getCurrentUser();
 
     DynamicForm form = factory.form().bindFromRequest();
     String learnerSolution = form.get("editorContent");
-    
+
+    // FIXME: Eigene Testdaten der Studierenden!
+
     // FIXME: evt. Speichern der Lösung und Laden bei erneuter Bearbeitung?
-    
+
     List<JsTestResult> testResults = JsCorrector.correct(JsExercise.finder.byId(exerciseId), learnerSolution);
-    
+
     if(request().acceptedTypes().get(0).toString().equals("application/json"))
       return ok(Json.toJson(testResults));
     else
       return ok(jscorrect.render(learnerSolution, testResults, user));
   }
-  
+
   public Result commitWeb(int exerciseId) {
     User user = UserManagement.getCurrentUser();
     DynamicForm form = factory.form().bindFromRequest();
     String learnerSolution = form.get("editorContent");
-    
+
     JsWebExercise exercise = JsWebExercise.finder.byId(exerciseId);
-    
+
     try {
       saveSolutionForUser(user, learnerSolution, exerciseId);
     } catch (IOException e) {
       Logger.error("Error while saving file ", e);
       return badRequest("Error while saving file!");
     }
-    
+
     String solutionUrl = "http://localhost:9000" + routes.Solution.site(user, "js", exercise.id).url();
-    
+
     List<JsWebTestResult> result = JsCorrector.correctWeb(exercise, solutionUrl);
-    
+
     if(request().acceptedTypes().get(0).toString().equals("application/json"))
       return ok(Json.toJson(result));
     else
       // FIXME: Implementierung Übersicht Endkorrektur Javascript Web!
       return ok("Diese Seite muss noch erstellt werden!");
   }
-  
+
   public Result exercise(int id) {
     User user = UserManagement.getCurrentUser();
     JsExercise exercise = JsExercise.finder.byId(id);
-    
+
     if(exercise == null)
       return badRequest(
           error.render(user, new Html("<p>Diese Aufgabe existert leider nicht.</p><p>Zur&uuml;ck zur <a href=\""
               + routes.JS.index() + "\">Startseite</a>.</p>")));
-    
+
     return ok(js.render(UserManagement.getCurrentUser(), exercise));
-    
+
   }
-  
+
   public Result exerciseWeb(int exerciseId) {
     User user = UserManagement.getCurrentUser();
     JsWebExercise exercise = JsWebExercise.finder.byId(exerciseId);
-    
+
     if(exercise == null)
       return badRequest(
           error.render(user, new Html("<p>Diese Aufgabe existert leider nicht.</p><p>Zur&uuml;ck zur <a href=\""
               + routes.JS.index() + "\">Startseite</a>.</p>")));
-    
+
     String oldSolution = exercise.declaration;
     try {
       Path file = util.getSolFileForExercise(user, "js", exerciseId, FILE_TYPE);
@@ -117,25 +119,25 @@ public class JS extends Controller {
     } catch (IOException e) {
       Logger.error("Error while loading old JsWeb solution: ", e);
     }
-    
+
     return ok(jsweb.render(user, exercise, oldSolution));
   }
-  
+
   public Result index() {
     User user = UserManagement.getCurrentUser();
     return ok(jsoverview.render(user, JsExercise.finder.all(), JsWebExercise.finder.all()));
   }
-  
+
   public Result validateTestData(int exerciseId) {
     JsExercise exercise = JsExercise.finder.byId(exerciseId);
     if(exercise == null)
       return badRequest();
-    
+
     DynamicForm form = factory.form().bindFromRequest();
-    
+
     int count = Integer.parseInt(form.get("count"));
     int inputCount = Integer.parseInt(form.get("inputs"));
-    
+
     List<CommitedTestData> testData = new ArrayList<>(count);
     for(int i = 0; i < count; i++) {
       List<String> input = new ArrayList<>(inputCount);
@@ -145,19 +147,19 @@ public class JS extends Controller {
       String output = form.get("outp" + i);
       testData.add(new CommitedTestData(i, input, output));
     }
-    
+
     JsCorrector.validateTestData(exercise, testData);
-    
+
     return ok(Json.toJson(testData));
   }
-  
+
   private void saveSolutionForUser(User user, String solution, int exercise) throws IOException {
     Path solDir = util.getSolDirForUserAndType(user, EXERCISE_TYPE);
     if(!Files.exists(solDir))
       Files.createDirectories(solDir);
-    
+
     Path saveTo = util.getSolFileForExercise(user, EXERCISE_TYPE, exercise, FILE_TYPE);
     Files.write(saveTo, Arrays.asList(solution), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
   }
-  
+
 }
--- modules/web/app/model/javascript/JsCorrector.java
+++ modules/web/app/model/javascript/JsCorrector.java
@@ -18,7 +18,7 @@ import model.javascript.web.JsWebTestResult;
 import play.Logger;
 
 public class JsCorrector {
-
+  
   public static List<JsTestResult> correct(JsExercise exercise, String learnerSolution) {
     ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
     try {
@@ -31,22 +31,21 @@ public class JsCorrector {
       Logger.error("Fehler beim Laden der Lernerlösung", e);
       return Arrays.asList(new JsTestResult(exercise.functionTests.get(0), Success.NONE, "", ""));
     }
-
+    
     // FIXME: Es gab einen Fehler beim Ausführen des Codes. Woher??
-
+    
     // Evaluiere Lernerlösung mit Testwerten
     return exercise.functionTests.stream().map(test -> test.evaluate(engine)).collect(Collectors.toList());
-
+    
   }
-
+  
   public static List<JsWebTestResult> correctWeb(JsWebExercise exercise, String solutionUrl) {
     WebDriver driver = loadWebSite(solutionUrl);
-
+    
     List<JsWebTestResult> results = exercise.tests.stream().map(test -> test.test(driver)).collect(Collectors.toList());
-
     return results;
   }
-
+  
   public static boolean validateResult(JsDataType type, Object gottenResult, String awaitedResult) {
     switch(type) {
     // FIXME: implement!!!!!
@@ -57,13 +56,13 @@ public class JsCorrector {
     default:
       return false;
     }
-
+    
   }
-
+  
   public static <T> boolean validateResult(T gottenResult, T awaitedResult) {
     return gottenResult.equals(awaitedResult);
   }
-
+  
   public static void validateTestData(JsExercise exercise, List<CommitedTestData> testData) {
     ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
     try {
@@ -73,12 +72,12 @@ public class JsCorrector {
       testData.forEach(data -> data.setOk(false));
       return;
     }
-
+    
     testData.forEach(data -> {
       try {
         String toEvaluate = exercise.buildToEvaluate(data.getInput());
         Object gottenResult = engine.eval(toEvaluate);
-
+        
         boolean validated = validateResult(exercise.returntype, gottenResult, data.getOutput());
         data.setOk(validated);
       } catch (ScriptException e) {
@@ -86,7 +85,7 @@ public class JsCorrector {
       }
     });
   }
-
+  
   private static WebDriver loadWebSite(String solutionUrl) {
     WebDriver driver = new HtmlUnitDriver(true);
     driver.get(solutionUrl);
