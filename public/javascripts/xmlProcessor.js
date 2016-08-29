27da39f350a25cf026e7097f428d59fc77348126
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-08-29 11:05:15 +0200

    Weitere Nutzung Auslagerung Editor

--- public/javascripts/xmlProcessor.js
+++ public/javascripts/xmlProcessor.js
@@ -1,35 +1,56 @@
 function processCorrection(correction) {
-  try {
-    var newResults = JSON.parse(correction);
-    
-    var numOfSuccessfulResults = 0;
-    
-    var resultsContainer = document.getElementById("element_result_container");
-    
-    // remove previous messages
-    while(resultsContainer.firstChild) {
-      resultsContainer.removeChild(resultsContainer.firstChild);
-    }
-    
-    for(i = 0; i < newResults.length; i++) {
-      handleResult(newResults[i], i);
-      if(newResults[i].errorType === "NONE") {
-        numOfSuccessfulResults++;
-      }
-    }
-    
-    var commitButton = document.getElementById("commit");
-    commitButton.disabled = false;
-    if(numOfSuccessfulResults == newResults.length) {
-      commitButton.title = "Sie k&ouml;nnen ihre L&ouml;sung abgeben.";
-      commitButton.className = "btn btn-success";
-    } else {
-      commitButton.title = "Sie k&ouml;nnen ihre L&ouml;sung abgeben, haben aber nicht alles richtig.";
-      commitButton.className = "btn btn-warning";
-    }
-  } catch(err) {
-    alert(err);
+  var newResults = JSON.parse(correction);
+  
+  var numOfSuccessfulResults = 0;
+  
+  var resultsContainer = document.getElementById("element_result_container");
+  
+  // remove previous messages
+  while(resultsContainer.firstChild) {
+    resultsContainer.removeChild(resultsContainer.firstChild);
   }
+  
+  for(i = 0; i < newResults.length; i++) {
+    handleResult(newResults[i], i);
+    if(newResults[i].errorType === "NONE") {
+      numOfSuccessfulResults++;
+    }
+  }
+  
+  var commitButton = document.getElementById("commit");
+  commitButton.disabled = false;
+  if(numOfSuccessfulResults == newResults.length) {
+    commitButton.title = "Sie k&ouml;nnen ihre L&ouml;sung abgeben.";
+    commitButton.className = "btn btn-success";
+  } else {
+    commitButton.title = "Sie k&ouml;nnen ihre L&ouml;sung abgeben, haben aber nicht alles richtig.";
+    commitButton.className = "btn btn-warning";
+  }
+}
+
+function prepareFormForSubmitting() {
+  document.getElementById("editorContent").value = editor.getValue();
+}
+
+function affirmFinalCommit() {
+  window.alert("Erfolgreich abgegeben!");
+}
+
+function testTheSolution(url) {
+  // AJAX-Objekt erstellen, Callback-Funktion bereitstellen
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if(xhttp.readyState == 4 && xhttp.status == 200) {
+      processCorrection(xhttp.responseText);
+    }
+  };
+  
+  // AJAX-Objekt mit Daten fuellen, absenden
+  var parameters = "editorContent=" + encodeURIComponent(editor.getValue());
+  xhttp.open("PUT", url, true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.setRequestHeader("Accept", "application/json");
+  xhttp.send(parameters);
 }
 
 function handleResult(result, resultIndex) {
