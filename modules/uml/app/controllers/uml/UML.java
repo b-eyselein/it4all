09c41fdcc48606b5258e3a7643b38eb69dad2df5
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-08-30 16:12:02 +0200

    Update UML: Initial Commit

---
+++ modules/uml/app/controllers/uml/UML.java
@@ -0,0 +1,12 @@
+package controllers.uml;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class UML extends Controller {
+
+  public Result index() {
+    return ok("TODO!");
+  }
+
+}
