a4c8466ba16f83aabfccedd32db57722b26c9101
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-09-08 13:03:42 +0200

    Kleines Refactoring + Syntaxfehleranzeige in JS

---
+++ modules/core/app/model/exercise/EvaluationFailed.java
@@ -0,0 +1,17 @@
+package model.exercise;
+
+public class EvaluationFailed extends EvaluationResult {
+
+  private String message;
+  
+  public EvaluationFailed(String theMessage) {
+    super(Success.NONE);
+    message = theMessage;
+  }
+  
+  @Override
+  public String getAsHtml() {
+    return "<div class=\"alert alert-dagner\">" + message + "</div>";
+  }
+
+}
