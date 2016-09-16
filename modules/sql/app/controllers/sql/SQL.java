9284fc2ac56366b93dc4b2b28daf886d7ffff784
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-09-16 13:23:56 +0200

    Fehler bei leerer Query in SQL behoben

--- modules/sql/app/controllers/sql/SQL.java
+++ modules/sql/app/controllers/sql/SQL.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -27,6 +28,7 @@ import play.data.FormFactory;
 import play.db.Database;
 import play.db.NamedDatabase;
 import play.libs.Json;
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security.Authenticated;
@@ -37,64 +39,64 @@ import views.html.sqloverview;
 
 @Authenticated(Secured.class)
 public class SQL extends Controller {
-  
+
   private static final String SHOW_ALL_TABLES = "SHOW TABLES";
   private static final String SELECT_ALL = "SELECT * FROM ";
-  
+
   @Inject
   @NamedDatabase("sqlselectuser")
   // IMPORTANT: DO NOT USE "DEFAULT" DATABASE
   private Database sql_select;
-  
+
   @Inject
   @NamedDatabase("sqlotheruser")
   private Database sql_other;
-  
+
   @Inject
   private FormFactory factory;
-  
+
   @Inject
   @SuppressWarnings("unused")
   private SqlStartUpChecker checker;
-  
+
   public Result commit(String scenarioName, String exerciseType, int exerciseId) {
     User user = UserManagement.getCurrentUser();
     SqlExercise exercise = SqlExercise.finder.byId(new SqlExerciseKey(scenarioName, exerciseId));
-    
+
     DynamicForm form = factory.form().bindFromRequest();
-    
+
     String learnerSolution = form.get("editorContent");
     FeedbackLevel feedbackLevel = FeedbackLevel.valueOf(form.get("feedbackLevel"));
-    
+
     if(learnerSolution.isEmpty())
-      return ok(Json.toJson(new EvaluationFailed("Sie haben eine leere Query abgegeben!")));
-    
+      return ok(Json.toJson(Arrays.asList(new EvaluationFailed("Sie haben eine leere Query abgegeben!"))));
+
     if(exercise == null)
-      return badRequest(Json.toJson(new EvaluationFailed("There is no such exercise!")));
-    
+      return badRequest(Json.toJson(Arrays.asList(new EvaluationFailed("There is no such exercise!"))));
+
     Database database = getDatabaseForExerciseType(exerciseType);
-    
+
     List<EvaluationResult> result = SqlCorrector.correct(database, user, learnerSolution, exercise, feedbackLevel);
-    
+
     JsonNode ret = Json.toJson(result);
-    
+
     return ok(Json.toJson(ret));
   }
-  
+
   public Result exercise(String scenarioName, String exerciseType, int exerciseId) {
     User user = UserManagement.getCurrentUser();
     SqlExercise exercise = SqlExercise.finder.byId(new SqlExerciseKey(scenarioName, exerciseId));
-    
+
     if(exercise == null)
       return badRequest("There is no such exercise!");
-    
+
     try {
       Connection connection = sql_select.getConnection();
       connection.setCatalog(scenarioName);
       List<SqlQueryResult> tables = new LinkedList<>();
       Statement statement = connection.createStatement();
       ResultSet existingDBs = statement.executeQuery(SHOW_ALL_TABLES);
-      
+
       while(existingDBs.next()) {
         String tableName = existingDBs.getString(1);
         Statement selectStatement = connection.createStatement();
@@ -102,7 +104,7 @@ public class SQL extends Controller {
         tables.add(new SqlQueryResult(tableResult, tableName));
         selectStatement.close();
       }
-      
+
       statement.close();
       connection.close();
       return ok(sqlexercise.render(user, exercise, tables));
@@ -111,15 +113,15 @@ public class SQL extends Controller {
       return badRequest(error.render(user, new Html("Fehler beim Auslesen der Tabellen!")));
     }
   }
-  
+
   public Result index() {
     return ok(sqloverview.render(UserManagement.getCurrentUser(), SqlScenario.finder.all()));
   }
-  
+
   private Database getDatabaseForExerciseType(String exerciseType) {
     if(exerciseType.equals("SELECT"))
       return sql_select;
     return sql_other;
   }
-  
+
 }
