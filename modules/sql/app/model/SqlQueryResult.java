75df9479b2d89f014ac02eb086815d14790fa14d
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-09-17 19:09:32 +0200

    Aktualisierung SqlQueryResult: SqlRow + SqlCell mit html-Darstellung

--- modules/sql/app/model/SqlQueryResult.java
+++ modules/sql/app/model/SqlQueryResult.java
@@ -4,50 +4,112 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SqlQueryResult {
   
-  private static boolean checkCells(SqlQueryResult first, SqlQueryResult second) {
-    List<List<String>> firstRows = first.getRows(), secondRows = second.getRows();
+  public static class SqlCell {
+    
+    private String content;
+    private boolean different = false;
+    
+    public SqlCell(String theContent) {
+      content = theContent;
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+      if(object == null || !(object instanceof SqlCell))
+        return false;
+      SqlCell other = (SqlCell) object;
+      return (content == null && other.content == null) || content.equals(other.content);
+    }
+    
+    public String getAsHtml() {
+      return "<td" + (different ? " class=\"danger\"" : "") + ">" + content + "</td>";
+    }
+    
+    public String getContent() {
+      return content;
+    }
+    
+    public void markAsDifferent() {
+      different = true;
+    }
+    
+  }
+  
+  public static class SqlRow {
+    
+    public Map<String, SqlCell> cells = new HashMap<>();
+    
+    public void addCell(String columnName, SqlCell cell) {
+      cells.put(columnName, cell);
+    }
+    
+    public String getAsHtml(List<String> columnNames) {
+      return "<tr>" + columnNames.stream().map(colName -> cells.get(colName).getAsHtml()).collect(Collectors.joining())
+          + "</tr>";
+    }
+    
+    public SqlCell getSqlCell(String columnName) {
+      return cells.get(columnName);
+    }
+    
+    public int size() {
+      return cells.size();
+    }
+    
+  }
+  
+  private static boolean checkCell(SqlCell firstCell, SqlCell secondCell) {
+    if((firstCell == null ^ secondCell == null) || !firstCell.equals(secondCell)) {
+      firstCell.markAsDifferent();
+      secondCell.markAsDifferent();
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+  private static boolean checkCells(SqlQueryResult first, SqlQueryResult second, List<String> columnNames) {
+    List<SqlRow> firstRows = first.getRows(), secondRows = second.getRows();
     if(firstRows.size() != secondRows.size())
       return false;
     
     int rowCount = firstRows.size();
+    boolean identic = true;
     for(int rowCounter = 0; rowCounter < rowCount; rowCounter++) {
-      List<String> firstRow = firstRows.get(rowCounter), secondRow = secondRows.get(rowCounter);
+      SqlRow firstRow = firstRows.get(rowCounter), secondRow = secondRows.get(rowCounter);
       if(firstRow.size() != secondRow.size())
         return false;
-      int cellCount = firstRow.size();
-      for(int cellCounter = 0; cellCounter < cellCount; cellCounter++) {
-        String firstCell = firstRow.get(cellCounter), secondCell = secondRow.get(cellCounter);
-        if(firstCell == null && secondCell == null)
-          continue;
-        if(firstCell == null || secondCell == null || !firstCell.equals(secondCell))
-          return false;
-      }
+      for(String colName: columnNames)
+        identic &= checkCell(firstRow.getSqlCell(colName), secondRow.getSqlCell(colName));
     }
     
-    return true;
+    return identic;
   }
   
-  private static boolean checkColumns(SqlQueryResult first, SqlQueryResult second) {
+  private static boolean checkColumnNames(SqlQueryResult first, SqlQueryResult second) {
     if(first.getColumnCount() != second.getColumnCount())
       return false;
+    
     int columnCount = first.getColumnCount();
     List<String> firstColNames = first.getColumnNames(), secondColNames = second.getColumnNames();
     for(int i = 0; i < columnCount; i++)
       if(!firstColNames.get(i).toLowerCase().equals(secondColNames.get(i).toLowerCase()))
         return false;
+      
     return true;
   }
   
   private List<String> colNames;
   
-  // TODO: evtl. eigene Klasse?
-  private List<List<String>> rows;
+  private List<SqlRow> rows;
   
   private String tableName = "";
   
@@ -61,9 +123,12 @@ public class SqlQueryResult {
     
     rows = new LinkedList<>();
     while(resultSet.next()) {
-      List<String> row = new ArrayList<>(columnCount);
-      for(int i = 1; i <= columnCount; i++)
-        row.add(resultSet.getString(colNames.get(i - 1)));
+      SqlRow row = new SqlRow();
+      for(int i = 1; i <= columnCount; i++) {
+        String columnName = colNames.get(i - 1);
+        String content = resultSet.getString(columnName);
+        row.addCell(columnName, new SqlCell(content));
+      }
       rows.add(row);
     }
     
@@ -82,7 +147,7 @@ public class SqlQueryResult {
     return colNames;
   }
   
-  public List<List<String>> getRows() {
+  public List<SqlRow> getRows() {
     return rows;
   }
   
@@ -91,18 +156,17 @@ public class SqlQueryResult {
   }
   
   public boolean isIdentic(SqlQueryResult other) {
-    return checkColumns(this, other) && checkCells(this, other);
+    return checkColumnNames(this, other) && checkCells(this, other, colNames);
   }
   
   public String toHtmlTable() {
-    String body = rows.stream().map(cells -> {
-      return cells.stream().collect(Collectors.joining("</td><td>", "<td>", "</td>"));
-    }).collect(Collectors.joining("</tr></td>", "<tr>", "</tr>"));
+    String body = rows.stream().map(row -> row.getAsHtml(colNames)).collect(Collectors.joining());
     
     String table = "<div class=\"table-responsive\">";
     table += "<table class=\"table table-bordered table-condensed\">";
     table += "<thead><tr><th>" + String.join("</th><th>", colNames) + "</th></tr></thead>";
     table += "<tbody>" + body + "</tbody></table></div>";
+    
     return table;
   }
   
