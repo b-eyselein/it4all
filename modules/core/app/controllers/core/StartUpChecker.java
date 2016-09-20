cdf0acde0aa4c843f6a7fd8d1112f74e65cedb76
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-09-20 15:59:30 +0200

    Hinzufügen Js-Aufgaben per JSON-Datei implementiert

---
+++ modules/core/app/controllers/core/StartUpChecker.java
@@ -0,0 +1,33 @@
+package controllers.core;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import play.Logger;
+import play.libs.Json;
+
+public abstract class StartUpChecker {
+
+  protected static Logger.ALogger theLogger = Logger.of("startup");
+
+  public StartUpChecker() {
+    performStartUpCheck();
+  }
+
+  protected abstract void performStartUpCheck();
+
+  protected JsonNode readJsonFile(Path path) {
+    try {
+      String fileAsString = String.join("\n", Files.readAllLines(path));
+      return Json.parse(fileAsString);
+    } catch (IOException e) {
+      theLogger.error("Error while reading file " + path, e);
+    }
+    return null;
+
+  }
+
+}
