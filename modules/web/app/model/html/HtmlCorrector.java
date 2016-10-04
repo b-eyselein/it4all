b507d05b941550a4443e90c8ee34eb9f3e8fd689
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-10-04 12:39:40 +0200

    Fix für fehlerhaftes Überschreiben der Punkte in Html/Grading

--- modules/web/app/model/html/HtmlCorrector.java
+++ modules/web/app/model/html/HtmlCorrector.java
@@ -14,46 +14,44 @@ import model.html.task.Task;
 import model.user.User;
 
 public class HtmlCorrector {
-  
+
   public static List<ElementResult> correct(String solutionUrl, HtmlExercise exercise, User student, String type) {
     WebDriver driver = loadWebSite(solutionUrl);
-    
+
     List<? extends Task> tasks = Collections.emptyList();
-    
+
     if(type.equals("html"))
       tasks = exercise.tasks;
     else if(type.equals("css"))
       tasks = exercise.cssTasks;
-    
+
     List<ElementResult> result = tasks.stream().map(task -> task.evaluate(driver)).collect(Collectors.toList());
-    
+
     if(type.equals("html"))
       saveGrading(exercise, student, calculatePoints(result));
-    
+
     return result;
   }
-  
+
   private static int calculatePoints(List<ElementResult> result) {
     return result.stream().mapToInt(res -> res.getPoints()).sum();
   }
-  
+
   private static WebDriver loadWebSite(String solutionUrl) {
     WebDriver driver = new HtmlUnitDriver();
     driver.get(solutionUrl);
     return driver;
   }
-  
+
   private static void saveGrading(HtmlExercise exercise, User user, int points) {
     GradingKey gradingKey = new GradingKey(user.name, exercise.id);
-    
     Grading grading = Grading.finder.byId(gradingKey);
     
     if(grading == null)
       grading = new Grading(gradingKey);
     
-    grading.points = points;
+    grading.setPoints(points);
     grading.save();
-    
   }
-  
+
 }
