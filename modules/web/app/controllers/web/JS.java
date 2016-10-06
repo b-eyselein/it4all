163d51d147d9990084728a08c24d2b5ef4ec8f48
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-10-06 12:22:36 +0200

    Implementierung Abgabe Js-Web

--- modules/web/app/controllers/web/JS.java
+++ modules/web/app/controllers/web/JS.java
@@ -16,12 +16,11 @@ import controllers.core.UserManagement;
 import model.Secured;
 import model.Util;
 import model.exercise.EvaluationResult;
+import model.javascript.CommitedTestData;
 import model.javascript.JsCorrector;
 import model.javascript.JsExercise;
 import model.javascript.JsExercise.JsDataType;
-import model.javascript.CommitedTestData;
 import model.javascript.web.JsWebExercise;
-import model.javascript.web.JsWebTestResult;
 import model.user.User;
 import play.Logger;
 import play.data.DynamicForm;
@@ -39,130 +38,129 @@ import views.html.javascript.jsweb;
 
 @Security.Authenticated(Secured.class)
 public class JS extends Controller {
-
+  
   private static final String EXERCISE_TYPE = "js";
   private static final String FILE_TYPE = "html";
-
+  
   private static List<CommitedTestData> extractAndValidateTestData(DynamicForm form, JsExercise exercise) {
     List<CommitedTestData> testData = new LinkedList<>();
-
+    
     int testCount = Integer.parseInt(form.get("count"));
     int inputCount = Integer.parseInt(form.get("inputs"));
-
+    
     List<JsDataType> dataTypes = exercise.getInputTypes();
-
+    
     for(int testCounter = 0; testCounter < testCount; testCounter++) {
       List<String> theInput = readTestDataFromForm(form, dataTypes, inputCount, testCounter);
       if(theInput != null)
         testData.add(new CommitedTestData(exercise, testCounter, theInput));
     }
-
+    
     JsCorrector.validateTestData(exercise, testData);
     return testData;
   }
-
+  
   private static List<String> readTestDataFromForm(DynamicForm form, List<JsDataType> dataTypes, int inputCount,
       int testCounter) {
     List<String> testData = new ArrayList<>(inputCount + 1);
     for(int inputCounter = 0; inputCounter < inputCount; inputCounter++) {
       String input = form.get("inp" + inputCounter + ":" + testCounter);
-
+      
       if(input == null || input.isEmpty())
         return null;
-
+      
       // TODO: Inputtype STRING, NUMBER... ?
       if(dataTypes.get(inputCounter) == JsDataType.STRING)
         input = "\"" + input + "\"";
-
+      
       testData.add(input);
     }
-
+    
     String output = form.get("outp" + testCounter);
     if(output == null || output.isEmpty())
       return null;
     testData.add(output);
-
+    
     return testData;
   }
-
+  
   @Inject
   private Util util;
-
+  
   @Inject
   private FormFactory factory;
-
+  
   public Result commit(int exerciseId) {
     User user = UserManagement.getCurrentUser();
-
+    
     JsExercise exercise = JsExercise.finder.byId(exerciseId);
     if(exercise == null)
       return badRequest(new Html("<p>Diese Aufgabe existiert nicht!</p><p>Zur&uuml;ck zur Startseite</p>"));
-
+    
     // Read commited solution and custom test data from request
     DynamicForm form = factory.form().bindFromRequest();
-
+    
     String learnerSolution = form.get("editorContent");
     List<CommitedTestData> userTestData = extractAndValidateTestData(form, exercise);
     // TODO: evtl. Anzeige aussortiertes TestDaten?
     userTestData = userTestData.stream().filter(data -> data.isOk()).collect(Collectors.toList());
-
+    
     // TODO: evt. Speichern der Lösung und Laden bei erneuter Bearbeitung?
-
+    
     List<EvaluationResult> testResults = JsCorrector.correct(exercise, learnerSolution, userTestData);
-
+    
     if(request().acceptedTypes().get(0).toString().equals("application/json"))
       return ok(Json.toJson(testResults));
     else
       return ok(jscorrect.render(learnerSolution, testResults, user));
   }
-
+  
   public Result commitWeb(int exerciseId) {
     User user = UserManagement.getCurrentUser();
     DynamicForm form = factory.form().bindFromRequest();
     String learnerSolution = form.get("editorContent");
-
+    
     JsWebExercise exercise = JsWebExercise.finder.byId(exerciseId);
-
+    
     try {
       saveSolutionForUser(user, learnerSolution, exerciseId);
     } catch (IOException e) {
       Logger.error("Error while saving file ", e);
       return badRequest("Error while saving file!");
     }
-
+    
     String solutionUrl = "http://localhost:9000" + routes.Solution.site(user, "js", exercise.id).url();
-
-    List<JsWebTestResult> testResults = JsCorrector.correctWeb(exercise, solutionUrl);
-
+    
+    List<EvaluationResult> testResults = JsCorrector.correctWeb(exercise, solutionUrl);
+    
     if(request().acceptedTypes().get(0).toString().equals("application/json"))
       return ok(Json.toJson(testResults));
     else
-      // FIXME: Implementierung Übersicht Endkorrektur Javascript Web!
-      return ok("Diese Seite muss noch erstellt werden!");
+      return ok(jscorrect.render(learnerSolution, testResults, user));
   }
-
+  
   public Result exercise(int id) {
     User user = UserManagement.getCurrentUser();
     JsExercise exercise = JsExercise.finder.byId(id);
-
+    
     if(exercise == null)
       return badRequest(
           error.render(user, new Html("<p>Diese Aufgabe existert leider nicht.</p><p>Zur&uuml;ck zur <a href=\""
               + routes.JS.index() + "\">Startseite</a>.</p>")));
-
+    
     return ok(js.render(UserManagement.getCurrentUser(), exercise));
-
+    
   }
-
+  
   public Result exerciseWeb(int exerciseId) {
     User user = UserManagement.getCurrentUser();
     JsWebExercise exercise = JsWebExercise.finder.byId(exerciseId);
-
+    
     if(exercise == null)
       return badRequest(
           error.render(user, new Html("<p>Diese Aufgabe existert leider nicht.</p><p>Zur&uuml;ck zur <a href=\""
               + routes.JS.index() + "\">Startseite</a>.</p>")));
-
+    
     String oldSolution = exercise.declaration;
     try {
       Path file = util.getSolFileForExercise(user, "js", exerciseId, FILE_TYPE);
@@ -171,33 +169,33 @@ public class JS extends Controller {
     } catch (IOException e) {
       Logger.error("Error while loading old JsWeb solution: ", e);
     }
-
+    
     return ok(jsweb.render(user, exercise, oldSolution));
   }
-
+  
   public Result index() {
     User user = UserManagement.getCurrentUser();
     return ok(jsoverview.render(user, JsExercise.finder.all(), JsWebExercise.finder.all()));
   }
-
+  
   public Result validateTestData(int exerciseId) {
     JsExercise exercise = JsExercise.finder.byId(exerciseId);
     if(exercise == null)
       return badRequest();
-
+    
     DynamicForm form = factory.form().bindFromRequest();
     List<CommitedTestData> testData = extractAndValidateTestData(form, exercise);
-
+    
     return ok(Json.toJson(testData));
   }
-
+  
   private void saveSolutionForUser(User user, String solution, int exercise) throws IOException {
     Path solDir = util.getSolDirForUserAndType(user, EXERCISE_TYPE);
     if(!Files.exists(solDir))
       Files.createDirectories(solDir);
-
+    
     Path saveTo = util.getSolFileForExercise(user, EXERCISE_TYPE, exercise, FILE_TYPE);
     Files.write(saveTo, Arrays.asList(solution), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
   }
-
+  
 }
--- modules/web/app/model/javascript/web/JsWebTestResult.java
+++ modules/web/app/model/javascript/web/JsWebTestResult.java
@@ -6,11 +6,11 @@ import model.exercise.EvaluationResult;
 import model.exercise.Success;
 
 public class JsWebTestResult extends EvaluationResult {
-  
+
   private JsWebTest test;
   private List<ConditionResult> postconditionResults;
   private List<ConditionResult> preconditionResults;
-  
+
   public JsWebTestResult(JsWebTest theTest, Success theSuccess, List<ConditionResult> thePreconditionResults,
       List<ConditionResult> thePostconditionResults) {
     super(theSuccess);
@@ -18,38 +18,38 @@ public class JsWebTestResult extends EvaluationResult {
     preconditionResults = thePreconditionResults;
     postconditionResults = thePostconditionResults;
   }
-  
+
   @Override
   public String getAsHtml() {
-    // FIXME: implement getAsHtml for JsWebTestReault
-    String toAdd = "<div class=\"panel panel-" + getBSClass() + "\">\n";
-    toAdd += "  <div class=\"panel-heading\">\n";
+    // TODO: restyling? getAsHtml for JsWebTestReault
+    String toAdd = "<div class=\"panel panel-" + getBSClass() + "\">";
+    toAdd += "  <div class=\"panel-heading\">";
     toAdd += "    <h4 class=\"panel-title\"><a data-toggle=\"collapse\" href=\"#col" + test.id + "\">Test " + (test.id)
-        + "</a></h4>\n";
-    toAdd += "  </div>\n";
-    
+        + "</a></h4>";
+    toAdd += "  </div>";
+
     toAdd += "  <div id=\"col" + test.id + "\" class=\"panel-collapse collapse"
-        + (success == Success.COMPLETE ? "" : " in") + "\">\n";
-    toAdd += "    <div class=\"panel-body\">\n";
-    
+        + (success == Success.COMPLETE ? "" : " in") + "\">";
+    toAdd += "    <div class=\"panel-body\">";
+
     for(ConditionResult result: preconditionResults)
       toAdd += result.getAsHtml();
-    
+
     for(ConditionResult result: postconditionResults)
       toAdd += result.getAsHtml();
-    
-    toAdd += "    </div>\n";
-    toAdd += "  </div>\n";
+
+    toAdd += "    </div>";
+    toAdd += "  </div>";
     toAdd += "</div>";
     return toAdd;
   }
-  
+
   public void postCondResult(List<ConditionResult> thePostconditionResults) {
     postconditionResults = thePostconditionResults;
   }
-  
+
   public void preCondResult(List<ConditionResult> thePreconditionResults) {
     preconditionResults = thePreconditionResults;
   }
-  
+
 }
