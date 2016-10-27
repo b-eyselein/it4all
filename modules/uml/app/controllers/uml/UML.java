91032cc88c7e19f242ce6fb98daf318702c461af
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-10-27 13:24:15 +0200

    Einbindung erste Version UML-Korrektur Klassendiagramme

--- modules/uml/app/controllers/uml/UML.java
+++ modules/uml/app/controllers/uml/UML.java
@@ -1,17 +1,45 @@
 package controllers.uml;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.inject.Inject;
 
 import controllers.core.ExerciseController;
+import controllers.core.UserManagement;
 import model.Util;
+import play.Logger;
 import play.data.FormFactory;
 import play.mvc.Result;
+import views.html.classselection;
+import views.html.diagramdrawing;
 
 public class UML extends ExerciseController {
   
   @Inject
   public UML(Util theUtil, FormFactory theFactory) {
     super(theUtil, theFactory);
+  }
+  
+  private static String getExerciseText() {
+    try {
+      Path file = Paths.get("modules/uml/conf/exerciseText.txt");
+      Logger.debug(file.toAbsolutePath().toString());
+      return String.join("\n", Files.readAllLines(file));
+      
+    } catch (IOException e) {
+      return "TODO!";
+    }
+  }
+  
+  public Result classSelection(int exerciseId) {
+    return ok(classselection.render(UserManagement.getCurrentUser()));
+  }
+  
+  public Result diagramDrawing(int exerciseId) {
+    return ok(diagramdrawing.render(UserManagement.getCurrentUser(), getExerciseText()));
   }
   
   public Result index() {
