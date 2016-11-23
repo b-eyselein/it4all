4537f07c907f1d97db808b74a3cbc9d017a48024
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2016-11-23 13:30:30 +0100

    Start Implementierung Auswertung der Evaluation

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -2,16 +2,22 @@ package controllers;
 
 import controllers.core.UserManagement;
 import model.AdminSecured;
+import model.feedback.Feedback;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 import views.html.admin;
+import views.html.evaluation.stats;
 
 @Security.Authenticated(AdminSecured.class)
 public class AdminController extends Controller {
   
   public Result index() {
     return ok(admin.render(UserManagement.getCurrentUser()));
+  }
+  
+  public Result stats() {
+    return ok(stats.render(UserManagement.getCurrentUser(), Feedback.finder.all()));
   }
   
 }
