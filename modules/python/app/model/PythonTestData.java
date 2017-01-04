42af2eeb0db0141f5982bea77ce29ecd4a696d3d
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2017-01-04 16:07:04 +0100

    Weitere Implementierung ProgLang

---
+++ modules/python/app/model/PythonTestData.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.List;
+
+import model.programming.ITestData;
+
+public class PythonTestData implements ITestData {
+
+  @Override
+  public String buildToEvaluate(String functionname) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int getId() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public List<String> getInput() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public String getOutput() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+}
