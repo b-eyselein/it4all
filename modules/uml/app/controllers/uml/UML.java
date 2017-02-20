1a7762ff54a1e5eb4948cbc390edae9027647c30
Kevin <k_fuchs@online.de>
2017-02-20 12:49:55 +0100

    json classselection

--- modules/uml/app/controllers/uml/UML.java
+++ modules/uml/app/controllers/uml/UML.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import controllers.core.ExerciseController;
 import controllers.core.UserManagement;
@@ -23,6 +29,8 @@ import play.data.FormFactory;
 import play.libs.Json;
 import play.mvc.Http.Request;
 import play.mvc.Result;
+import scala.collection.immutable.List;
+import scala.util.parsing.json.JSONArray;
 import views.html.classselection;
 import views.html.diagramdrawing;
 import views.html.umloverview;
@@ -51,14 +59,138 @@ public class UML extends ExerciseController<IntExerciseIdentifier> {
   }
   
   public Result correct() {
-    // Correct classes...
     DynamicForm form = factory.form().bindFromRequest();
     String classes = form.get("fname");
     JsonNode node = Json.parse(classes);
     node.get("classes");
+    JsonNode node_c = Json.parse("{\"classes\":[\"Canikuji\",\"Firma\"],\"methods\":[\"Hersteller\",\"Fotosystems\"],\"attributes\":[\"Ikonograf\",\"Webseite\"]}");
     Logger.debug(Json.prettyPrint(node));
+    Logger.debug("getclasses: "+Json.prettyPrint(node.get("classes")));
+    Logger.debug("getmethods: "+Json.prettyPrint(node.get("methods")));
+    Logger.debug("getattr: "+Json.prettyPrint(node.get("attributes")));
+   // Init
+    String[] arr_classes = node.get("classes").toString().substring(1, node.get("classes").toString().length()-1).split(",");
+    String[] arr_methods = node.get("methods").toString().substring(1, node.get("methods").toString().length()-1).split(",");
+    String[] arr_attributes = node.get("attributes").toString().substring(1, node.get("attributes").toString().length()-1).split(",");
+    String[] arr_classes_c = node_c.get("classes").toString().substring(1, node_c.get("classes").toString().length()-1).split(",");
+    String[] arr_methods_c = node_c.get("methods").toString().substring(1, node_c.get("methods").toString().length()-1).split(",");
+    String[] arr_attributes_c = node_c.get("attributes").toString().substring(1, node_c.get("attributes").toString().length()-1).split(",");
+
+    String json ="{\"correct\":[{\"classes\":[";    
+    //correct
+    ArrayList<String> al_classes= new ArrayList<String>(Arrays.asList(arr_classes));
+    ArrayList<String> al_classes_c= new ArrayList<String>(Arrays.asList(arr_classes_c)); 
+    for (String object: al_classes) {
+    	if(al_classes_c.contains(object)){
+    		Logger.debug("Klasse: "+object+" stimmt.");
+    		json+=object+",";
+    	}
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    json+="],\"methods\":[";
+    ArrayList<String> al_methods= new ArrayList<String>(Arrays.asList(arr_methods));
+    ArrayList<String> al_methods_c= new ArrayList<String>(Arrays.asList(arr_methods_c)); 
+    for (String object: al_methods) {
+    	if(al_methods_c.contains(object)){
+    		Logger.debug("Methode: "+object+" stimmt.");
+    		json+=object+",";
+    	}
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    json+="],\"attributes\":[";
+    ArrayList<String> al_attributes= new ArrayList<String>(Arrays.asList(arr_attributes));
+    ArrayList<String> al_attributes_c= new ArrayList<String>(Arrays.asList(arr_attributes_c)); 
+    for (String object: al_attributes) {
+    	if(al_attributes_c.contains(object)){
+    		Logger.debug("Attribut: "+object+" stimmt.");
+    		json+=object+",";
+    	}
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    //false
+    json+="]}],\"false\":[{\"classes\":[";
+    for (String object: al_classes) {
+    	if(!al_classes_c.contains(object))
+    	json+=object+",";
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    json+="],\"methods\":[";
+    for (String object: al_methods) {
+    	if(!al_methods_c.contains(object))
+    	json+=object+",";
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    json+="],\"attributes\":[";
+    for (String object: al_attributes) {
+    	if(!al_attributes_c.contains(object))
+    	json+=object+",";
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    //missing
+    json+="]}],\"missing\":[{\"classes\":[";
+    for (String object: al_classes_c) {
+    	if(!al_classes.contains(object))
+    	json+=object+",";
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    json+="],\"methods\":[";
+    for (String object: al_methods_c) {
+    	if(!al_methods.contains(object))
+    	json+=object+",";
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    json+="],\"attributes\":[";
+    for (String object: al_attributes_c) {
+    	if(!al_attributes.contains(object))
+    	json+=object+",";
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    // optional
+    /*
+    json+="]}],\"optional\":[{\"classes\":[";
+    for (String object: al_classes_c) {
+    	json+=object+",";
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    json+="],\"methods\":[";
+    for (String object: al_methods_c) {
+    	json+=object+",";
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    json+="],\"attributes\":[";
+    for (String object: al_attributes_c) {
+    	json+=object+",";
+    }
+    if(json.endsWith(",")){
+    	json=json.substring(0, json.length()-1);
+    }
+    */
+    json+="]}]}";
     
-    return ok("HALLO!");
+    Logger.debug(json);
+    return ok("ok");
   }
 
   public Result diagramDrawing(IntExerciseIdentifier identifier) {
--- modules/uml/app/controllers/uml/ValidationUtils.java
+++ modules/uml/app/controllers/uml/ValidationUtils.java
@@ -1,4 +1,4 @@
-package model;
+package controllers.uml;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
