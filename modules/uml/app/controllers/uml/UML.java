26a30aabb8a4319960906687d28a354cd426e3ad
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2017-03-10 11:36:50 +0100

    Small Refactorings to UML

--- modules/uml/app/controllers/uml/UML.java
+++ modules/uml/app/controllers/uml/UML.java
@@ -1,36 +1,28 @@
 package controllers.uml;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import javax.inject.Inject;
-import com.fasterxml.jackson.databind.JsonNode;
 import controllers.core.ExerciseController;
 import controllers.core.UserManagement;
 import model.UmlClassselection;
 import model.UmlDiagramdrawing;
 import model.UmlExercise;
-import model.UmlDiagramdrawing_Connection;
-import model.UmlDiagramdrawing_Class;
 import model.Util;
 import model.result.CompleteResult;
 import model.user.User;
 import play.Logger;
 import play.data.DynamicForm;
 import play.data.FormFactory;
-import play.libs.Json;
-import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.Request;
-import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
 
 public class UML extends ExerciseController {
-	
-	private static final String FORM_VALUE = "learnerSolution";
+
+  private static final String FORM_VALUE = "learnerSolution";
 
   @Inject
   public UML(Util theUtil, FormFactory theFactory) {
@@ -52,49 +44,54 @@ public class UML extends ExerciseController {
     return ok(views.html.classselection.render(UserManagement.getCurrentUser()));
   }
 
-  public Result correctclassselection() {   
-    DynamicForm form = factory.form().bindFromRequest();
-    String classes = form.get("fname");
-    if(classes == null || classes.isEmpty())return badRequest("Keine Daten übertragen!");;
-    UmlClassselection ue = new UmlClassselection(classes);
-    return ok(views.html.solution_classselection.render(UserManagement.getCurrentUser(),ue));
-  }
-  
-  public Result correctdiagramdrawinghelp() throws IOException {   
-	    DynamicForm form = factory.form().bindFromRequest();
-	    String json = form.get("fname");
-	    Logger.debug("diagramdrawhinghelp(): "+json);
-	    if(json == null || json.isEmpty())return badRequest("Keine Daten übertragen!");;
-	    UmlDiagramdrawing ue =new UmlDiagramdrawing(json);
-	    return ok(views.html.solution_diagramdrawinghelp.render(UserManagement.getCurrentUser(),ue));
-  }
-  
-  public Result correctdiagramdrawing() throws IOException {   
-	    DynamicForm form = factory.form().bindFromRequest();
-	    String json = form.get("fname");
-	    Logger.debug("diagramdrawhinghelp(): "+json);
-	    if(json == null || json.isEmpty())return badRequest("Keine Daten übertragen!");;
-	    UmlDiagramdrawing ue =new UmlDiagramdrawing(json);
-	    return ok(views.html.solution_diagramdrawing.render(UserManagement.getCurrentUser(),ue));
-}
-  
-  
   @Override
   protected CompleteResult correct(Request request, User user, int exerciseId) {
     // TODO Auto-generated method stub
     return null;
   }
 
+  public Result correctclassselection() {
+    DynamicForm form = factory.form().bindFromRequest();
+    String classes = form.get("fname");
+    if(classes == null || classes.isEmpty())
+      return badRequest("Keine Daten übertragen!");
+    ;
+    UmlClassselection ue = new UmlClassselection(classes);
+    return ok(views.html.solution_classselection.render(UserManagement.getCurrentUser(), ue));
+  }
+
+  public Result correctdiagramdrawing() throws IOException {
+    DynamicForm form = factory.form().bindFromRequest();
+    String json = form.get("fname");
+    Logger.debug("diagramdrawhinghelp(): " + json);
+    if(json == null || json.isEmpty())
+      return badRequest("Keine Daten übertragen!");
+
+    UmlDiagramdrawing ue = new UmlDiagramdrawing(json);
+    return ok(views.html.solution_diagramdrawing.render(UserManagement.getCurrentUser(), ue));
+  }
+
+  public Result correctdiagramdrawinghelp() throws IOException {
+    DynamicForm form = factory.form().bindFromRequest();
+    String json = form.get("fname");
+    Logger.debug("diagramdrawhinghelp(): " + json);
+    if(json == null || json.isEmpty())
+      return badRequest("Keine Daten übertragen!");
+    
+    UmlDiagramdrawing ue = new UmlDiagramdrawing(json);
+    return ok(views.html.solution_diagramdrawinghelp.render(UserManagement.getCurrentUser(), ue));
+  }
+
   public Result diagramDrawing(int exerciseId) {
     return ok(views.html.diagramdrawing.render(UserManagement.getCurrentUser(), getExerciseText()));
   }
 
+  public Result diagramDrawingHelp(int exerciseId) {
+    return ok(views.html.diagramdrawinghelp.render(UserManagement.getCurrentUser()));
+  }
+
   public Result diff(int exerciseId) {
     return ok(views.html.difficulty.render(UserManagement.getCurrentUser()));
-  }
-  
-  public Result diagramDrawingHelp(int exerciseId) {
-	  return ok(views.html.diagramdrawinghelp.render(UserManagement.getCurrentUser()));
   }
 
   public Result index() {
--- modules/uml/app/model/UmlClassselection.java
+++ modules/uml/app/model/UmlClassselection.java
@@ -7,25 +7,24 @@ import com.fasterxml.jackson.databind.JsonNode;
 import play.libs.Json;
 
 public class UmlClassselection {
-
-  private String title;	
-  private ArrayList<String>  classes_c;
-  private ArrayList<String>  classes_f;
-  private ArrayList<String>  classes_m;
-  private ArrayList<String>  methods_c;
-  private ArrayList<String>  methods_f;
-  private ArrayList<String>  methods_m;
-  private ArrayList<String>  attributes_c;
-  private ArrayList<String>  attributes_f;
-  private ArrayList<String>  attributes_m;
-
-
+  
+  private String title;
+  private ArrayList<String> classes_c;
+  private ArrayList<String> classes_f;
+  private ArrayList<String> classes_m;
+  private ArrayList<String> methods_c;
+  private ArrayList<String> methods_f;
+  private ArrayList<String> methods_m;
+  private ArrayList<String> attributes_c;
+  private ArrayList<String> attributes_f;
+  private ArrayList<String> attributes_m;
   
   public UmlClassselection(String input) {
-	setTitleExcercise("Foto");
+    setTitleExcercise("Foto");
     JsonNode node = Json.parse(input);
     JsonNode node_c = Json.parse(
         "{\"classes\":[\"Telekonverter\",\"Profigehäuse\",\"Kameragehäuse\",\"Amateurgehäuse\",\"Profiblitz\",\"Objektiv\",\"Amateurblitz\",\"Sonnenblende\",\"Zoomobjektiv\",\"Festweitenobjektiv\"],\"methods\":[\"Hersteller\",\"Fotosystems\"],\"attributes\":[\"Ikonograf\",\"Webseite\"]}");
+
     // Init
     String[] arr_classes = node.get("classes").toString().substring(1, node.get("classes").toString().length() - 1)
         .split(",");
@@ -39,158 +38,169 @@ public class UmlClassselection {
         .substring(1, node_c.get("methods").toString().length() - 1).split(",");
     String[] arr_attributes_c = node_c.get("attributes").toString()
         .substring(1, node_c.get("attributes").toString().length() - 1).split(",");
+
     // correct
-    ArrayList<String> al_classes = new ArrayList<String>(Arrays.asList(arr_classes));
-    ArrayList<String> al_classes_c = new ArrayList<String>(Arrays.asList(arr_classes_c));
-    ArrayList<String> classes_c= new ArrayList<>();
+    ArrayList<String> al_classes = new ArrayList<>(Arrays.asList(arr_classes));
+    ArrayList<String> al_classes_c = new ArrayList<>(Arrays.asList(arr_classes_c));
+    ArrayList<String> classes_c = new ArrayList<>();
+
     for(String object: al_classes) {
       if(al_classes_c.contains(object)) {
         classes_c.add(object);
       }
     }
-    this.classes_c=classes_c;
+
+    this.classes_c = classes_c;
     ArrayList<String> al_methods = new ArrayList<String>(Arrays.asList(arr_methods));
     ArrayList<String> al_methods_c = new ArrayList<String>(Arrays.asList(arr_methods_c));
-    ArrayList<String> methods_c= new ArrayList<>();
+    ArrayList<String> methods_c = new ArrayList<>();
     for(String object: al_methods) {
       if(al_methods_c.contains(object)) {
         methods_c.add(object);
       }
     }
-    this.methods_c=methods_c;
+    this.methods_c = methods_c;
     ArrayList<String> al_attributes = new ArrayList<String>(Arrays.asList(arr_attributes));
     ArrayList<String> al_attributes_c = new ArrayList<String>(Arrays.asList(arr_attributes_c));
-    ArrayList<String> attributes_c= new ArrayList<>();
+    ArrayList<String> attributes_c = new ArrayList<>();
     for(String object: al_attributes) {
       if(al_attributes_c.contains(object)) {
         attributes_c.add(object);
       }
     }
-    this.attributes_c=attributes_c;
-    ArrayList<String> classes_f= new ArrayList<>();
+    this.attributes_c = attributes_c;
+    ArrayList<String> classes_f = new ArrayList<>();
     for(String object: al_classes) {
-      if(!al_classes_c.contains(object)){
-          classes_f.add(object);
+      if(!al_classes_c.contains(object)) {
+        classes_f.add(object);
       }
-    this.classes_f=classes_f;
+      this.classes_f = classes_f;
     }
-    ArrayList<String> methods_f= new ArrayList<>();
+    ArrayList<String> methods_f = new ArrayList<>();
     for(String object: al_methods) {
-      if(!al_methods_c.contains(object)){
-          methods_f.add(object);
+      if(!al_methods_c.contains(object)) {
+        methods_f.add(object);
       }
-    this.methods_f=methods_f;
+      this.methods_f = methods_f;
     }
-    ArrayList<String> attributes_f= new ArrayList<>();
+    ArrayList<String> attributes_f = new ArrayList<>();
     for(String object: al_attributes) {
-      if(!al_attributes_c.contains(object)){
-          attributes_f.add(object);
+      if(!al_attributes_c.contains(object)) {
+        attributes_f.add(object);
       }
     }
-    this.attributes_f=attributes_f;
+    this.attributes_f = attributes_f;
     // missing
-    ArrayList<String> classes_m= new ArrayList<>();
+    ArrayList<String> classes_m = new ArrayList<>();
     for(String object: al_classes_c) {
-      if(!al_classes.contains(object)){
-          classes_m.add(object);
+      if(!al_classes.contains(object)) {
+        classes_m.add(object);
       }
     }
-    this.classes_m=classes_m;
-    ArrayList<String> methods_m= new ArrayList<>();
+    this.classes_m = classes_m;
+    ArrayList<String> methods_m = new ArrayList<>();
     for(String object: al_methods_c) {
-      if(!al_methods.contains(object)){
-          methods_m.add(object);
+      if(!al_methods.contains(object)) {
+        methods_m.add(object);
       }
     }
-    this.methods_m=methods_m;
-    ArrayList<String> attributes_m= new ArrayList<>();
+    this.methods_m = methods_m;
+    ArrayList<String> attributes_m = new ArrayList<>();
     for(String object: al_attributes_c) {
-      if(!al_attributes.contains(object)){
-          attributes_m.add(object);
+      if(!al_attributes.contains(object)) {
+        attributes_m.add(object);
       }
     }
-    this.attributes_m=attributes_m;
+    this.attributes_m = attributes_m;
   }
   
-  public String getClasses_c(){
-	  String ret="";
-	  Iterator<String> it = this.classes_c.listIterator(0);
-	  while(it.hasNext()){
-		  ret+=it.next()+"\n";
-	  }
-	  return ret;
-  }
-  public String getClasses_f(){
-	  String ret="";
-	  Iterator<String> it = this.classes_f.listIterator(0);
-	  while(it.hasNext()){
-		  ret+=it.next()+"\n";
-	  }
-	  return ret;
-  }
-  public String getClasses_m(){
-	  String ret="";
-	  Iterator<String> it = this.classes_m.listIterator(0);
-	  while(it.hasNext()){
-		  ret+=it.next()+"\n";
-	  }
-	  return ret;
-  }
-  public String getMethods_c(){
-	  String ret="";
-	  Iterator<String> it = this.methods_c.listIterator(0);
-	  while(it.hasNext()){
-		  ret+=it.next()+"\n";
-	  }
-	  return ret;
-  }
-  public String getMethods_f(){
-	  String ret="";
-	  Iterator<String> it = this.methods_f.listIterator(0);
-	  while(it.hasNext()){
-		  ret+=it.next()+"\n";
-	  }
-	  return ret;
-  }
-  public String getMethods_m(){
-	  String ret="";
-	  Iterator<String> it = this.methods_m.listIterator(0);
-	  while(it.hasNext()){
-		  ret+=it.next()+"\n";
-	  }
-	  return ret;
-  }
-  public String getAttributes_c(){
-	  String ret="";
-	  Iterator<String> it = this.attributes_c.listIterator(0);
-	  while(it.hasNext()){
-		  ret+=it.next()+"\n";
-	  }
-	  return ret;
-  }
-  public String getAttributes_f(){
-	  String ret="";
-	  Iterator<String> it = this.attributes_f.listIterator(0);
-	  while(it.hasNext()){
-		  ret+=it.next()+"\n";
-	  }
-	  return ret;
-  }
-  public String getAttributes_m(){
-	  String ret="";
-	  Iterator<String> it = this.attributes_m.listIterator(0);
-	  while(it.hasNext()){
-		  ret+=it.next()+"\n";
-	  }
-	  return ret;
+  public String getAttributes_c() {
+    String ret = "";
+    Iterator<String> it = this.attributes_c.listIterator(0);
+    while(it.hasNext()) {
+      ret += it.next() + "\n";
+    }
+    return ret;
   }
   
-  public void setTitleExcercise(String title){
-	  this.title=title;
-  }
-  public String getTitleExercise(){
-	  return title;
+  public String getAttributes_f() {
+    String ret = "";
+    Iterator<String> it = this.attributes_f.listIterator(0);
+    while(it.hasNext()) {
+      ret += it.next() + "\n";
+    }
+    return ret;
   }
   
+  public String getAttributes_m() {
+    String ret = "";
+    Iterator<String> it = this.attributes_m.listIterator(0);
+    while(it.hasNext()) {
+      ret += it.next() + "\n";
+    }
+    return ret;
+  }
+  
+  public String getClasses_c() {
+    String ret = "";
+    Iterator<String> it = this.classes_c.listIterator(0);
+    while(it.hasNext()) {
+      ret += it.next() + "\n";
+    }
+    return ret;
+  }
+  
+  public String getClasses_f() {
+    String ret = "";
+    Iterator<String> it = this.classes_f.listIterator(0);
+    while(it.hasNext()) {
+      ret += it.next() + "\n";
+    }
+    return ret;
+  }
+  
+  public String getClasses_m() {
+    String ret = "";
+    Iterator<String> it = this.classes_m.listIterator(0);
+    while(it.hasNext()) {
+      ret += it.next() + "\n";
+    }
+    return ret;
+  }
+  
+  public String getMethods_c() {
+    String ret = "";
+    Iterator<String> it = this.methods_c.listIterator(0);
+    while(it.hasNext()) {
+      ret += it.next() + "\n";
+    }
+    return ret;
+  }
+  
+  public String getMethods_f() {
+    String ret = "";
+    Iterator<String> it = this.methods_f.listIterator(0);
+    while(it.hasNext()) {
+      ret += it.next() + "\n";
+    }
+    return ret;
+  }
+  
+  public String getMethods_m() {
+    String ret = "";
+    Iterator<String> it = this.methods_m.listIterator(0);
+    while(it.hasNext()) {
+      ret += it.next() + "\n";
+    }
+    return ret;
+  }
+  
+  public String getTitleExercise() {
+    return title;
+  }
+  
+  public void setTitleExcercise(String title) {
+    this.title = title;
+  }
   
 }
