07c57e9ec98b3e112b6c84cf478930da43860df6
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2017-03-27 15:59:23 +0200

    Kleiner Fix

--- modules/sql/app/model/correctionresult/CreateResult.java
+++ modules/sql/app/model/correctionresult/CreateResult.java
@@ -9,18 +9,18 @@ import model.matching.MatchingResult;
 import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
 
 public class CreateResult extends MatchingResult<ColumnDefinition, ColumnDefinitionMatch> {
-
+  
   public CreateResult(List<ColumnDefinitionMatch> theMatches, List<ColumnDefinition> theNotMatchedInFirst,
       List<ColumnDefinition> theNotMatchedInSecond) {
     super(FeedbackLevel.MINIMAL_FEEDBACK, theMatches, theNotMatchedInFirst, theNotMatchedInSecond);
   }
-  
+
   public String getAsHtml() {
     String ret = "<div class=\"col-md-6\">";
     ret += "<div class=\"panel panel-" + getBSClass() + "\">";
-
+    
     ret += "<div class=\"panel-heading\">Vergleich der definierten Spalten</div>";
-
+    
     ret += "<div class=\"panel-body\">";
     // Not defined columns
     if(!notMatchedInSecond.isEmpty())
@@ -29,24 +29,24 @@ public class CreateResult extends MatchingResult<ColumnDefinition, ColumnDefinit
           + "</div>";
     else
       ret += "<div class=\"alert alert-success\">Es wurden alle nötigen Spalten definiert.</div>";
-
+    
     // Surplus columns
     if(!notMatchedInFirst.isEmpty())
       ret += "<div class=\"alert alert-danger\">Folgende überzähligen Spalten wurden definiert: "
-          + notMatchedInFirst.stream().map(colDef -> colDef.getColumnName())
+          + notMatchedInFirst.stream().map(ColumnDefinition::getColumnName)
               .collect(Collectors.joining("</li><li>", "<ul><li>", "</li></ul>"))
           + "</div>";
     else
       ret += "<div class=\"alert alert-success\">Es wurden keine überschüssigen Spalten definiert.</div>";
-
+    
     ret += "<hr>";
-
+    
     // Columns with correct name
     // for(EvaluationResult colRes: matches)
     // ret += colRes.getAsHtml();
-
+    
     ret += "</div></div></div>";
     return ret;
   }
-
+  
 }
