39f8b176e23f3d41042280e86eb5375af51de021
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2017-05-11 16:48:58 +0200

    Zwischencommit Question Update

--- modules/question/app/model/question/FreetextQuestion.java
+++ modules/question/app/model/question/FreetextQuestion.java
@@ -10,31 +10,36 @@ import model.FreetextAnswer;
 
 @Entity
 public class FreetextQuestion extends Question {
-
+  
   // TODO: Exercise extends Model...
   public static final com.avaje.ebean.Model.Finder<Integer, FreetextQuestion> finder = new com.avaje.ebean.Model.Finder<>(
       FreetextQuestion.class);
-
+  
   @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
   List<FreetextAnswer> givenAnswers;
-
+  
   public FreetextQuestion(int theId) {
     super(theId);
   }
-
+  
   @Override
   public boolean isFreetext() {
     return true;
   }
-
+  
   @Override
   public void saveInDb() {
     save();
+  }
+  
+  @Override
+  public String toString() {
+    return "Autor: " + author + ", Text: " + text + ", Titel: " + title;
   }
 
   @Override
   public boolean userHasAnswered(String username) {
     return givenAnswers.stream().anyMatch(ans -> ans.key.username.equals(username));
   }
-
+  
 }
