00befd426e862aaaf8f993615354b9f8068f1a78
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2017-06-27 15:53:01 +0200

    Loggin-Statement entfernt

--- modules/binary/app/model/NAryResult.java
+++ modules/binary/app/model/NAryResult.java
@@ -1,30 +1,27 @@
 package model;
 
-import play.Logger;
-
 public abstract class NAryResult {
-  
-  public static final String VALUE = "value";
 
+  public static final String VALUE = "value";
+  
   protected NAryNumber targetNumber;
   protected NAryNumber learnerSolution;
-
+  
   public NAryResult(NAryNumber theTargetNumber, NAryNumber theLearnerSolution) {
     targetNumber = theTargetNumber;
     learnerSolution = theLearnerSolution;
   }
-
+  
   public boolean checkSolution() {
-    Logger.debug(targetNumber.getValue() + " :: " + learnerSolution.getValue());
     return targetNumber.getValue() == learnerSolution.getValue();
   }
-
+  
   public NAryNumber getLearnerSolution() {
     return learnerSolution;
   }
-
+  
   public NAryNumber getTargetNumber() {
     return targetNumber;
   }
-
+  
 }
