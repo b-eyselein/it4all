c533da283f5d47571e0c95d1fd64d5a795dae758
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2017-07-10 10:17:14 +0200

    Fix SQL

--- modules/sql/app/controllers/sql/SqlController.java
+++ modules/sql/app/controllers/sql/SqlController.java
@@ -38,12 +38,12 @@ import play.db.NamedDatabase;
 import play.mvc.Result;
 
 public class SqlController extends BaseExerciseController {
-  
+
   private static final String STANDARD_SQL = "";
-  
+
   private Database sqlSelect;
   private Database sqlOther;
-  
+
   @Inject
   public SqlController(FormFactory theFactory, @NamedDatabase("sqlselectroot") Database theSqlSelect,
       @NamedDatabase("sqlotherroot") Database theSqlOther) {
@@ -51,17 +51,17 @@ public class SqlController extends BaseExerciseController {
     sqlSelect = theSqlSelect;
     sqlOther = theSqlOther;
   }
-  
+
   public static User getUser() {
     User user = ExerciseController.getUser();
-    
+
     if(SqlUser.finder.byId(user.name) == null)
       // Make sure there is a corresponding entrance in other db...
       new SqlUser(user.name).save();
-    
+
     return user;
   }
-  
+
   private static SqlSample findBestFittingSample(String userStatement, List<SqlSample> samples) {
     return samples.parallelStream().min((samp1, samp2) -> {
       int dist1 = Levenshtein.distance(samp1.sample, userStatement);
@@ -69,7 +69,7 @@ public class SqlController extends BaseExerciseController {
       return dist1 - dist2;
     }).orElse(samples.get(0));
   }
-  
+
   private static List<String> readExistingTables(Connection connection) {
     try(ResultSet existingTables = connection.createStatement().executeQuery(StringConsts.SHOW_ALL_TABLES)) {
       final List<String> tableNames = new LinkedList<>();
@@ -81,7 +81,7 @@ public class SqlController extends BaseExerciseController {
       return Collections.emptyList();
     }
   }
-  
+
   private static SqlQueryResult readTableContent(Connection connection, String tableName) {
     try(PreparedStatement selectStatement = connection.prepareStatement(StringConsts.SELECT_ALL_DUMMY + tableName)) { // NOSONAR
       final ResultSet tableResult = selectStatement.executeQuery();
@@ -91,7 +91,7 @@ public class SqlController extends BaseExerciseController {
       return null;
     }
   }
-  
+
   private static List<SqlQueryResult> readTablesInDatabase(Database db, String databaseName) {
     try(Connection connection = db.getConnection()) {
       connection.setCatalog(databaseName);
@@ -102,101 +102,99 @@ public class SqlController extends BaseExerciseController {
       return Collections.emptyList();
     }
   }
-  
+
   private static void saveSolution(String userName, String learnerSolution, SqlExerciseKey exKey) {
     SqlSolutionKey key = new SqlSolutionKey(userName, exKey);
-    
+
     SqlSolution solution = SqlSolution.finder.byId(key);
     if(solution == null)
       solution = new SqlSolution(key);
-    
+
     solution.sol = learnerSolution;
     solution.save();
   }
-  
+
   public Result correct(int scenarioId, int exerciseId) {
     User user = getUser();
     try {
       SqlResult<? extends Statement, ?> result = correct(user.name, scenarioId, exerciseId);
-      
+
       log(user, new ExerciseCompletionEvent(request(), exerciseId, result.getResults()));
-      
+
       return ok(views.html.correction.render("SQL", result.render(), result.getLearnerSolution(), user,
           routes.SqlController.index()));
     } catch (CorrectionException e) { // NOSONAR
-      return ok(
-          views.html.error.render(user, "<div class=\"alert alert-danger\">Es gab einen Fehler bei der Korrektur: <pre>"
-              + e.getStackTrace() + "</pre></div>"));
+      Logger.error("FEHLER:", e);
+      return ok(views.html.error.render(user, e));
     }
   }
-  
+
   public Result correctLive(int scenarioId, int exerciseId) {
     try {
       User user = getUser();
-      
+
       SqlResult<? extends Statement, ?> result = correct(user.name, scenarioId, exerciseId);
-      
+
       log(user, new ExerciseCorrectionEvent(request(), exerciseId, result.getResults()));
-      
+
       return ok(result.render());
     } catch (CorrectionException e) { // NOSONAR
-      return ok(views.html.correctionerror
-          .render("<p>Es gab einen Fehler bei der Korrektur:</p><pre>" + e.getMessage() + "</pre>"));
+      return ok(views.html.correctionError.render(e));
     }
   }
-  
+
   public Result exercise(int scenarioId, int exerciseId) {
     User user = getUser();
-    
+
     SqlExerciseKey exKey = new SqlExerciseKey(scenarioId, exerciseId);
     SqlExercise exercise = SqlExercise.finder.byId(exKey);
-    
+
     if(exercise == null)
       return redirect(controllers.sql.routes.SqlController.index());
-    
+
     List<SqlQueryResult> tables = readTablesInDatabase(sqlSelect, exercise.scenario.shortName);
-    
+
     SqlSolution oldSol = SqlSolution.finder.byId(new SqlSolutionKey(user.name, exKey));
     String oldOrDefSol = oldSol == null ? STANDARD_SQL : oldSol.sol;
-    
+
     log(user, new ExerciseStartEvent(request(), exerciseId));
-    
+
     return ok(views.html.sqlExercise.render(user, exercise, oldOrDefSol, tables));
   }
-  
+
   public Result filteredScenario(int id, String exType, int start) {
     final SqlScenario scenario = SqlScenario.finder.byId(id);
-    
+
     if(scenario == null)
       return redirect(controllers.sql.routes.SqlController.index());
-    
+
     return ok(views.html.sqlscenario.render(getUser(), scenario, SqlExerciseType.valueOf(exType), start));
   }
-  
+
   public Result index() {
     return ok(views.html.sqlIndex.render(getUser(), SqlScenario.finder.all()));
   }
-  
+
   public Result scenarioes() {
     return ok(views.html.scenarioes.render(getUser(), SqlScenario.finder.all()));
   }
-  
+
   private SqlResult<? extends Statement, ?> correct(String userName, int scenarioId, int exerciseId)
       throws CorrectionException {
     String learnerSol = factory.form().bindFromRequest().get(StringConsts.FORM_VALUE);
-    
+
     if(learnerSol == null || learnerSol.isEmpty())
       throw new CorrectionException(learnerSol, StringConsts.EMPTY_SOLUTION);
-    
+
     SqlExercise exercise = SqlExercise.finder.byId(new SqlExerciseKey(scenarioId, exerciseId));
-    
+
     saveSolution(userName, learnerSol, exercise.key);
-    
+
     SqlSample sample = findBestFittingSample(learnerSol, exercise.samples);
-    
+
     return exercise.getCorrector().correct(getDBForExType(exercise.exerciseType), learnerSol, sample, exercise);
   }
-  
+
   private Database getDBForExType(SqlExerciseType exerciseType) {
     return exerciseType == SqlExerciseType.SELECT ? sqlSelect : sqlOther;
   }
--- modules/sql/app/model/querycorrectors/select/SelectCorrector.java
+++ modules/sql/app/model/querycorrectors/select/SelectCorrector.java
@@ -32,112 +32,111 @@ import play.db.Database;
 
 @Singleton
 public class SelectCorrector extends QueryCorrector<Select, SelectItem> {
-  
+
   private static final SelectColumnMatcher COL_MATCHER = new SelectColumnMatcher();
-  
+
   private static final GroupByMatcher GROUP_BY_MATCHER = new GroupByMatcher();
-  
+
   private static final OrderByMatcher ORDER_BY_MATCHER = new OrderByMatcher();
-  
+
   public SelectCorrector() {
     super("SELECT");
   }
-  
+
   protected static MatchingResult<Expression, Match<Expression>> compareGroupByElements(Select plainUserQuery,
       Select plainSampleQuery) {
     List<Expression> group1 = ((PlainSelect) plainUserQuery.getSelectBody()).getGroupByColumnReferences();
     List<Expression> group2 = ((PlainSelect) plainSampleQuery.getSelectBody()).getGroupByColumnReferences();
-    
+
     if(group1 == null && group2 == null)
       return null;
-    
+
     return GROUP_BY_MATCHER.match(group1 != null ? group1 : Collections.emptyList(),
         group2 != null ? group2 : Collections.emptyList());
   }
-  
+
   protected static MatchingResult<OrderByElement, OrderByMatch> compareOrderByElements(Select plainUserQuery,
       Select plainSampleQuery) {
     List<OrderByElement> order1 = ((PlainSelect) plainUserQuery.getSelectBody()).getOrderByElements();
     List<OrderByElement> order2 = ((PlainSelect) plainSampleQuery.getSelectBody()).getOrderByElements();
-    
+
     if(order1 == null && order2 == null)
       return null;
-    
+
     return ORDER_BY_MATCHER.match(order1 != null ? order1 : Collections.emptyList(),
         order2 != null ? order2 : Collections.emptyList());
   }
-  
+
   private SqlQueryResult executeStatement(Select select, Connection conn) throws CorrectionException {
     try(Statement statement = conn.createStatement()) {
       return new SqlQueryResult(statement.executeQuery(select.toString()));
     } catch (SQLException e) {
-      throw new CorrectionException(select.toString(), "There has been an error while running a query", e);
+      throw new CorrectionException(select.toString(), "Es gab einen Fehler bei der Ausführung des Statements ", e);
     }
   }
-  
+
   @Override
   protected MatchingResult<SelectItem, ColumnMatch<SelectItem>> compareColumns(Select userQuery,
       Map<String, String> userTableAliases, Select sampleQuery, Map<String, String> sampleTableAliases) {
     return COL_MATCHER.match(StringConsts.COLUMNS_NAME, getColumns(userQuery), getColumns(sampleQuery));
   }
-  
+
   @Override
   protected SqlExecutionResult executeQuery(Database database, Select userStatement, Select sampleStatement,
       SqlExercise exercise) throws CorrectionException {
     SqlQueryResult userResult = null;
     SqlQueryResult sampleResult = null;
-    
+
     try(Connection conn = database.getConnection()) {
       conn.setCatalog(exercise.scenario.shortName);
-      
+
       userResult = executeStatement(userStatement, conn);
-      
+
       sampleResult = executeStatement(sampleStatement, conn);
-      
+
       return new SqlExecutionResult(userResult, sampleResult);
     } catch (SQLException e) {
-      e.fillInStackTrace();
       throw new CorrectionException(userStatement.toString(), e.getMessage(), e);
     }
-    
+
   }
-  
+
   protected List<SelectItem> getColumns(Select select) {
     return ((PlainSelect) select.getSelectBody()).getSelectItems();
   }
-  
+
   @Override
   protected List<String> getTableNames(Select select) {
     return getTables(select).stream().map(Table::getName).collect(Collectors.toList());
   }
-  
+
   @Override
   protected List<Table> getTables(Select query) {
     List<Table> tables = new LinkedList<>();
-    
+
     PlainSelect plain = (PlainSelect) query.getSelectBody();
-    
+
     if(plain.getFromItem() instanceof Table)
       tables.add((Table) plain.getFromItem());
-    
+
     if(plain.getJoins() != null)
       plain.getJoins().stream().filter(join -> join.getRightItem() instanceof Table)
           .forEach(join -> tables.add((Table) join.getRightItem()));
-    
+
     return tables;
-    
+
   }
-  
+
   @Override
   protected Expression getWhere(Select select) {
     return ((PlainSelect) select.getSelectBody()).getWhere();
   }
-  
+
   @Override
   protected SqlResult<Select, SelectItem> instantiateResult(String learnerSolution) {
     return new SqlResult<>(learnerSolution);
   }
-  
+
   @Override
   protected List<MatchingResult<? extends Object, ? extends Match<? extends Object>>> makeOtherComparisons(Select userQ,
       Select sampleQ) {
@@ -145,5 +144,5 @@ public class SelectCorrector extends QueryCorrector<Select, SelectItem> {
     MatchingResult<OrderByElement, OrderByMatch> orderByComparison = compareOrderByElements(userQ, sampleQ);
     return Arrays.asList(groupByComparison, orderByComparison);
   }
-  
+
 }
