f2f7f4d97dc3311c119d1120bc8c9087716966ce
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2017-09-07 14:08:52 +0200

    Tags für WebExercise

--- modules/web/app/model/WebExercise.java
+++ modules/web/app/model/WebExercise.java
@@ -14,48 +14,47 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.ebean.Finder;
 import model.exercise.Exercise;
-import model.exercise.Tag;
 import model.task.HtmlTask;
 import model.task.JsWebTask;
 import play.twirl.api.Html;
 
 @Entity
 public class WebExercise extends Exercise {
-
+  
   public static final Finder<Integer, WebExercise> finder = new Finder<>(WebExercise.class);
-
+  
   @Column(columnDefinition = "text")
   public String htmlText;
-
+  
   @Column(columnDefinition = "text")
   public String jsText;
-
+  
   @OneToMany(mappedBy = "exercise", cascade = CascadeType.ALL)
   @JsonManagedReference
   public List<HtmlTask> htmlTasks;
-
+  
   @OneToMany(mappedBy = "exercise", cascade = CascadeType.ALL)
   @JsonManagedReference
   public List<JsWebTask> jsTasks;
-
+  
   @OneToMany(mappedBy = "exercise", cascade = CascadeType.ALL)
   @JsonIgnore
   public List<WebSolution> solutions;
-
+  
   public WebExercise(int id) {
     super(id);
   }
-
+  
   @JsonIgnore
   public static long withHtmlPart() {
     return finder.all().stream().filter(ex -> !ex.htmlTasks.isEmpty()).count();
   }
-
+  
   @JsonIgnore
   public static long withJsPart() {
     return finder.all().stream().filter(ex -> !ex.jsTasks.isEmpty()).count();
   }
-
+  
   @JsonProperty("htmlText")
   public List<String> getHtmlTextForJson() {
     return SPLITTER.splitToList(htmlText);
@@ -67,14 +66,14 @@ public class WebExercise extends Exercise {
   }
   
   @Override
-  public List<? extends Tag> getTags() {
-    return Arrays.asList(WebTag.W_HTML, WebTag.W_JS);
+  public List<WebTag> getTags() {
+    return Arrays.asList(new WebTag("Html", !htmlTasks.isEmpty()), new WebTag("Js", !jsTasks.isEmpty()));
   }
-
+  
   @Override
   @JsonIgnore
   public Html renderRest() {
     return views.html.webAdmin.webExTableRest.render(this);
   }
-
+  
 }
---
+++ modules/web/app/model/WebTag.scala
@@ -0,0 +1,13 @@
+package model;
+
+import model.exercise.Tag
+
+class WebTag(part: String, hasExes: Boolean) extends Tag {
+
+  override def cssClass = if (hasExes) "label label-primary" else "label label-default"
+
+  override def getButtonContent() = part
+
+  override def getTitle = s"Diese Aufgabe besitzt ${if(!hasExes) "k" else ""}einen $part-Teil"; 
+  
+}
