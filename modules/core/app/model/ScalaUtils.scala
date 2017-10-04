ee27c864986b681ea00f8d9e96ead7623a2ccca0
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2017-10-04 17:04:02 +0200

    Update XmlExerciseReader auf Scala

--- modules/core/app/model/ScalaUtils.scala
+++ modules/core/app/model/ScalaUtils.scala
@@ -1,23 +1,41 @@
 package model
 
-import scala.util.Try
-import scala.util.Failure
-import scala.util.Success
+import java.io.IOException
+import java.nio.file.{ CopyOption, Files, Path }
+
+import scala.util.{ Failure, Success, Try }
+import java.nio.file.OpenOption
 
 object ScalaUtils {
 
-  def cleanly[A, B](resource: A)(cleanup: A => Unit)(doWork: A => B): Try[B] = {
+  def copyFile(source: Path, target: Path, options: CopyOption): Try[Path] = {
+    try {
+      Success(Files.copy(source, target, options))
+    } catch {
+      case e: IOException ⇒ Failure(e)
+    }
+  }
+
+  def writeFile(target: Path, content: java.util.List[String], openOption: OpenOption): Try[Path] = {
+    try {
+      Success(Files.write(target, content, openOption))
+    } catch {
+      case e: IOException ⇒ Failure(e)
+    }
+  }
+
+  def cleanly[A, B](resource: A)(cleanup: A ⇒ Unit)(doWork: A ⇒ B): Try[B] = {
     try {
       Success(doWork(resource))
     } catch {
-      case e: Exception => Failure(e)
+      case e: Exception ⇒ Failure(e)
     } finally {
       try {
         if (resource != null) {
           cleanup(resource)
         }
       } catch {
-        case e: Exception => println(e) // should be logged
+        case e: Exception ⇒ println(e) // should be logged
       }
     }
   }
