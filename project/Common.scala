7628221a38238492054cb1aef4df65ca107da946
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2017-10-06 10:22:32 +0200

    Update auf MySQL 82

--- project/Common.scala
+++ project/Common.scala
@@ -12,7 +12,7 @@ object Common {
   // Scalatest
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.1" % "test"
 
-  val mysqlDependency = "mysql" % "mysql-connector-java" % "6.0.6"
+  val mysqlDependency = "mysql" % "mysql-connector-java" % "8.0.8-dmr"
 
   val seleniumDep = "org.seleniumhq.selenium" % "selenium-java" % "3.5.2"
   val htmlUnitDep = "org.seleniumhq.selenium" % "selenium-htmlunit-driver" % "2.52.0"
