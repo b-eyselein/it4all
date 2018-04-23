2af4d905df668f044df1a380e237c9204cd40a3c
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2018-04-23 12:32:17 +0200

    Kleine Fixes Aufgaben Prog AD, Uml KD

--- conf/resources/programming/5-hospital/base.py
+++ conf/resources/programming/5-hospital/base.py
@@ -1,49 +1,16 @@
 from typing import List
 
 
-class Medikament:
-    def __init__(self, id: str, name: str):
-        self.id = id
-        self.name = name
-
-
-class Rezept:
-    def __init__(self, id: int, medikamente: List[Medikament]):
-        self.id = id
-        self.medikamente = medikamente
-
-
-class Person:
+class Patient:
     def __init__(self, id: str, name: str, alter: int):
         self.id = id
         self.name = name
         self.alter = alter
 
 
-class Patient(Person):
-    def __init__(self, id: str, name: str, alter: int, rezepte: List[Rezept]):
-        super().__init__(id, name, alter)
-        self.rezepte = rezepte
-
-
-class Krankenschwester(Person):
-    def __init__(self, id: str, name: str, alter: int):
-        super().__init__(id, name, alter)
-
-
-class Doktor(Person):
-    def __init__(self, id: str, name: str, alter: int, patienten: List[Patient], rezepte: List[Rezept]):
-        super().__init__(id, name, alter)
-        self.patienten = patienten
-        self.rezepte = rezepte
-
-
 class Station:
-    def __init__(self, nummer: int, doktoren: List[Doktor], krankenschwestern: List[Krankenschwester],
-                 patienten: List[Patient]):
+    def __init__(self, nummer: int, patienten: List[Patient]):
         self.nummer = nummer
-        self.doktoren = doktoren
-        self.krankenschwestern = krankenschwestern
         self.patienten = patienten
 
 # {1}
