ead72d8f012004c6652ced7db424c22276ffb26b
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2018-07-02 15:31:05 +0200

    Commented functions deleted

--- app/model/persistence/SingleExerciseTableDefs.scala
+++ app/model/persistence/SingleExerciseTableDefs.scala
@@ -38,18 +38,6 @@ trait SingleExerciseTableDefs[Ex <: Exercise, CompEx <: CompleteEx[Ex], SolType,
 
   def futureUserCanSolvePartOfExercise(username: String, exerciseId: Int, part: PartType): Future[Boolean] = Future(true)
 
-  //  protected def futureResultForUserExAndPart(username: String, exerciseId: Int, part: PartType): Future[Option[PartResultType]] = Future(None)
-
-  //    db.run(resultsForPartsTable.filter(r => r.username === username && r.exerciseId === exerciseId && r.part === part).result.headOption)
-
-  //  def futureSaveResult(username: String, exerciseId: Int, part: PartType, points: Double, maxPoints: Double): Future[Boolean] = Future(false)
-
-  //    db.run(resultsForPartsTable insertOrUpdate WebResultForPart(username, exerciseId, part, points, maxPoints)) map (_ => true) recover {
-  //      case e: Throwable =>
-  //        Logger.error("Error while updating result: ", e)
-  //        false
-  //    }
-
   // Abstract table definitions
 
   protected abstract class PartSolutionsTable(tag: Tag, name: String) extends Table[DBSolType](tag, name) {
