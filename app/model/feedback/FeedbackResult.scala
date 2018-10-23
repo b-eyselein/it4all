a09e2b96401800c59d8d3f2d48aa36d24ea41f09
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2018-10-23 16:29:58 +0200

    Rework Evaluationsauswertung

--- app/model/feedback/FeedbackResult.scala
+++ app/model/feedback/FeedbackResult.scala
@@ -1,9 +1,9 @@
 package model.feedback
 
-import model.EvaluatedAspect
-import model.EvaluatedAspects._
-import model.Mark
+import model.{EvaluatedAspect, Mark}
 import model.Mark.NoMark
+
+import scala.language.postfixOps
 
 final case class FeedbackResult(tool: String, allFeedback: Seq[Feedback]) {
 
@@ -18,13 +18,18 @@ final case class FeedbackResult(tool: String, allFeedback: Seq[Feedback]) {
 
 object FeedbackResult {
 
-  def avg(feedback: Map[Mark, Int]): Int = {
-    val marksWithoutNoMark: Map[Mark, Int] = feedback filter {
+  def avgAndCount(feedback: Map[Mark, Int]): (Double, Int) = {
+    val marksWithoutNoMark: Seq[(Mark, Int)] = feedback filter {
       case (mark, _) => mark != Mark.NoMark
-    }
+    } toSeq
 
-    if (marksWithoutNoMark.isEmpty) 0
-    else marksWithoutNoMark.map { case (f, m) => f.value * m }.toSeq.sum / marksWithoutNoMark.size
+    marksWithoutNoMark match {
+      case Seq() => (0d, 0)
+      case marks =>
+        val markCount: Int = marks.map { case (_, count) => count }.sum
+        val avgMark: Double = math.rint(marks.map { case (mark, count) => mark.value * count }.sum.toDouble / markCount * 100) / 100d
+        (avgMark, markCount)
+    }
   }
 
 }
