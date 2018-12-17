25b318289a480ac134ca68977d39e385be34293d
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2018-12-17 15:27:31 +0100

    Small refactoring ProgCorrector

--- app/model/programming/ProgCorrector.scala
+++ app/model/programming/ProgCorrector.scala
@@ -22,15 +22,29 @@ object ProgCorrector {
   def correct(user: User, exercise: ProgCompleteEx, language: ProgLanguage, implementation: String, completeTestData: Seq[TestData],
               toolMain: ProgToolMain)(implicit ec: ExecutionContext): Future[Try[ProgCompleteResult]] = {
 
+
     val exerciseResourcesFolder: File = toolMain.exerciseResourcesFolder / (exercise.ex.id + "-" + exercise.ex.folderIdentifier)
     val solutionTargetDir: File = toolMain.solutionDirForExercise(user.username, exercise.ex.id)
     val testDataFileContent: String = Json.prettyPrint(TestDataJsonFormat.dumpTestDataToJson(exercise, completeTestData))
 
-    writeAndCopyFiles(solutionTargetDir, language.fileEnding, implementation, testDataFileContent)
+    // Write/Create files
+    val solFileName = solutionFileName(language.fileEnding)
+    val solutionFile = solutionTargetDir / solFileName
+    solutionFile.write(implementation)
 
-    // FIXME: what if image does not exist?
+    val testDataFile = solutionTargetDir / testDataFileName
+    testDataFile.write(testDataFileContent)
+
+    val resultFile = solutionTargetDir / resultFileName
+    resultFile.createIfNotExists(createParents = true)
+
+
+    /*
+     * FIXME: what if image does not exist?
+     *  --> val imageExists: Boolean = DockerConnector.imageExists(DockerPullsStartTask.pythonProgTesterImage)
+     */
     val containerResult: Future[RunContainerResult] = DockerConnector.runContainer(DockerPullsStartTask.pythonProgTesterImage,
-      dockerBinds = mountProgrammingFiles(exerciseResourcesFolder, solutionTargetDir, fileEnding = "py") //, deleteContainerAfterRun = false
+      dockerBinds = mountProgrammingFiles(exerciseResourcesFolder, solutionTargetDir, solFileName, fileEnding = "py") //, deleteContainerAfterRun = false
     )
 
     val futureResults = containerResult map {
@@ -46,29 +60,15 @@ object ProgCorrector {
     }
   }
 
-  private def writeAndCopyFiles(solTargetDir: File, fileEnding: String, impl: String, testDataFileContent: String): (File, File, File) = {
-
-    // FIXME: evaluate files!
-
-    val solutionWrite = (solTargetDir / solutionFileName(fileEnding)).write(impl)
-
-    val testDataWrite = (solTargetDir / testDataFileName).write(testDataFileContent)
-
-    val resultFileCreate = (solTargetDir / resultFileName).createIfNotExists(createParents = true)
-
-    (solutionWrite, testDataWrite, resultFileCreate)
-  }
-
-  private def mountProgrammingFiles(exResFolder: File, solTargetDir: File, fileEnding: String): Seq[DockerBind] = {
+  private def mountProgrammingFiles(exResFolder: File, solTargetDir: File, solFileName: String, fileEnding: String): Seq[DockerBind] = {
 
     // FIXME: update with files in exerciseResourcesFolder!
     val testMainFileName = buildTestMainFileName(fileEnding)
-    val testMainMount = DockerBind(exResFolder / testMainFileName, DockerConnector.DefaultWorkingDir / testMainFileName)
+    val testMainMount = DockerBind(exResFolder / testMainFileName, DockerConnector.DefaultWorkingDir / testMainFileName, isReadOnly = true)
 
-    val solFileName = solutionFileName(fileEnding)
-    val solutionMount = DockerBind(solTargetDir / solFileName, DockerConnector.DefaultWorkingDir / solFileName)
+    val solutionMount = DockerBind(solTargetDir / solFileName, DockerConnector.DefaultWorkingDir / solFileName, isReadOnly = true)
 
-    val testDataMount = DockerBind(solTargetDir / testDataFileName, DockerConnector.DefaultWorkingDir / testDataFileName)
+    val testDataMount = DockerBind(solTargetDir / testDataFileName, DockerConnector.DefaultWorkingDir / testDataFileName, isReadOnly = true)
 
     val resultFileMount = DockerBind(solTargetDir / resultFileName, DockerConnector.DefaultWorkingDir / resultFileName)
 
