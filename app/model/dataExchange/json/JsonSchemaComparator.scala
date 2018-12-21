abe1323f3a49c51859890b559711f4d856d49634
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2018-12-21 09:16:47 +0100

    Refactor JsonSchemaComparator

--- app/model/dataExchange/json/JsonSchemaComparator.scala
+++ app/model/dataExchange/json/JsonSchemaComparator.scala
@@ -4,71 +4,76 @@ import com.eclipsesource.schema.{SchemaArray, SchemaInteger, SchemaNull, SchemaN
 
 object JsonSchemaComparator {
 
-  def compare(userSchema: SchemaType, sampleSchema: SchemaType): JsonSchemaComparisonResult = (userSchema, sampleSchema) match {
-    case (userObj: SchemaObject, sampleObj: SchemaObject) => compareObject(userObj, sampleObj)
-
-    case (userString: SchemaString, sampleString: SchemaString) => compareString(userString, sampleString)
-
-    case (userNum: SchemaNumber, sampleNum: SchemaNumber) => compareNumber(userNum, sampleNum)
-
-    case (userInt: SchemaInteger, sampleInt: SchemaInteger) => compareInt(userInt, sampleInt)
-
-    case (userNull: SchemaNull, sampleNull: SchemaNull) => compareNull(userNull, sampleNull)
-
-    case (userArray: SchemaArray, sampleArray: SchemaArray) => compareArray(userArray, sampleArray)
-
-    case (userTuple: SchemaTuple, sampleTuple: SchemaTuple) => compareTuple(userTuple, sampleTuple)
-
-    case (userOther, sampleOther) =>
-      println(userOther.getClass + " :: " + sampleOther.getClass)
+  def compare(userSchema: SchemaType, sampleSchema: SchemaType): JsonSchemaComparisonResult = sampleSchema match {
+    case sampleObj: SchemaObject    => compareObject(userSchema, sampleObj)
+    case sampleString: SchemaString => compareString(userSchema, sampleString)
+    case sampleNum: SchemaNumber    => compareNumber(userSchema, sampleNum)
+    case sampleInt: SchemaInteger   => compareInt(userSchema, sampleInt)
+    case sampleNull: SchemaNull     => compareNull(userSchema, sampleNull)
+    case sampleArray: SchemaArray   => compareArray(userSchema, sampleArray)
+    case sampleTuple: SchemaTuple   => compareTuple(userSchema, sampleTuple)
+    case sampleOther                =>
+      println(userSchema.getClass + " :: " + sampleOther.getClass)
       SchemaTypesDifferent
   }
 
-  private def compareObject(userObject: SchemaObject, sampleObject: SchemaObject): JsonSchemaComparisonResult = {
-    val propertiesMatchingResult = SchemaPropMatcher.doMatch(userObject.properties, sampleObject.properties)
+  private def compareObject(userSchemaType: SchemaType, sampleObject: SchemaObject): JsonSchemaComparisonResult = userSchemaType match {
+    case userObject: SchemaObject =>
+      val propertiesMatchingResult = SchemaPropMatcher.doMatch(userObject.properties, sampleObject.properties)
 
-    /*
-     * TODO: compare constraints!
-     *  -> println(userObject.constraints)
-     *  -> println(sampleObject.constraints)
-     */
+      /*
+       * TODO: compare constraints!
+       *  -> println(userObject.constraints)
+       *  -> println(sampleObject.constraints)
+       */
 
-    SchemaObjectComparisonResult(userObject, sampleObject, propertiesMatchingResult)
+      SchemaObjectComparisonResult(userObject, sampleObject, propertiesMatchingResult)
+    case _                        => SchemaTypesDifferent
   }
 
-  private def compareString(userString: SchemaString, sampleString: SchemaString): JsonSchemaComparisonResult = {
-    /*
-     * TODO: compare constraints!
-     *  -> println(userString.constraints)
-     *  -> println(sampleString.constraints)
-     */
+  private def compareString(userSchemaType: SchemaType, sampleString: SchemaString): JsonSchemaComparisonResult = userSchemaType match {
+    case userString: SchemaString =>
+      /*
+       * TODO: compare constraints!
+       *  -> println(userString.constraints)
+       *  -> println(sampleString.constraints)
+       */
 
-    SchemaStringComparisonResult(userString, sampleString)
+      SchemaStringComparisonResult(userString, sampleString)
+    case _                        => SchemaTypesDifferent
   }
 
-  private def compareNumber(userNumber: SchemaNumber, sampleNumber: SchemaNumber): JsonSchemaComparisonResult = {
-    // TODO: more comparisons...
-    userNumber.constraints
+  private def compareNumber(userSchemaType: SchemaType, sampleNumber: SchemaNumber): JsonSchemaComparisonResult = userSchemaType match {
+    case userNumber: SchemaNumber =>
+      /*
+       * TODO: more comparisons...
+       *  userNumber.constraints
+       */
 
-    SchemaNumberComparisonResult(userNumber, sampleNumber)
+      SchemaNumberComparisonResult(userNumber, sampleNumber)
+    case _                        => SchemaTypesDifferent
   }
 
-  private def compareInt(userInteger: SchemaInteger, sampleInteger: SchemaInteger): JsonSchemaComparisonResult = {
-    SchemaIntegerComparisonResult(userInteger, sampleInteger)
+  private def compareInt(userSchemaType: SchemaType, sampleInteger: SchemaInteger): JsonSchemaComparisonResult = userSchemaType match {
+    case userInteger: SchemaInteger => SchemaIntegerComparisonResult(userInteger, sampleInteger)
+    case _                          => SchemaTypesDifferent
   }
 
-  private def compareNull(userNull: SchemaNull, sampleNull: SchemaNull): JsonSchemaComparisonResult = {
-    SchemaNullComparisonResult(userNull, sampleNull)
+  private def compareNull(userSchemaType: SchemaType, sampleNull: SchemaNull): JsonSchemaComparisonResult = userSchemaType match {
+    case userNull: SchemaNull => SchemaNullComparisonResult(userNull, sampleNull)
+    case _                    => SchemaTypesDifferent
   }
 
-  private def compareArray(userArray: SchemaArray, sampleArray: SchemaArray): JsonSchemaComparisonResult = {
-    val itemComparison = JsonSchemaComparator.compare(userArray.item, sampleArray.item)
-
-    SchemaArrayComparisonResult(userArray, sampleArray, itemComparison)
+  private def compareArray(userSchemaType: SchemaType, sampleArray: SchemaArray): JsonSchemaComparisonResult = userSchemaType match {
+    case userArray: SchemaArray =>
+      val itemComparison = JsonSchemaComparator.compare(userArray.item, sampleArray.item)
+      SchemaArrayComparisonResult(userArray, sampleArray, itemComparison)
+    case _                      => SchemaTypesDifferent
   }
 
-  private def compareTuple(userTuple: SchemaTuple, sampleTuple: SchemaTuple): JsonSchemaComparisonResult = {
-    SchemaTupleComparisonResult(userTuple, sampleTuple)
+  private def compareTuple(userSchemaType: SchemaType, sampleTuple: SchemaTuple): JsonSchemaComparisonResult = userSchemaType match {
+    case userTuple: SchemaTuple => SchemaTupleComparisonResult(userTuple, sampleTuple)
+    case _                      => SchemaTypesDifferent
   }
 
 }
