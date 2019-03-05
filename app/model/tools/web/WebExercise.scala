2fdf1a5f59f22da5f10ef162354906c97af1f30d
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2019-03-05 14:31:18 +0100

    Remove class WebExTag

--- app/model/tools/web/WebExercise.scala
+++ app/model/tools/web/WebExercise.scala
@@ -9,18 +9,6 @@ import play.twirl.api.Html
 import scala.collection.immutable.IndexedSeq
 import scala.language.postfixOps
 
-sealed trait JsActionType extends EnumEntry
-
-object JsActionType extends PlayEnum[JsActionType] {
-
-  override val values: IndexedSeq[JsActionType] = findValues
-
-  case object CLICK extends JsActionType
-
-  case object FILLOUT extends JsActionType
-
-}
-
 final case class WebCollection(id: Int, title: String, author: String, text: String, state: ExerciseState, shortName: String)
   extends ExerciseCollection
 
@@ -64,6 +52,19 @@ final case class HtmlTask(id: Int, text: String, xpathQuery: String, textContent
 
 }
 
+sealed trait JsActionType extends EnumEntry
+
+object JsActionType extends PlayEnum[JsActionType] {
+
+  override val values: IndexedSeq[JsActionType] = findValues
+
+  case object CLICK extends JsActionType
+
+  case object FILLOUT extends JsActionType
+
+}
+
+
 final case class JsTask(id: Int, text: String, xpathQuery: String, actionType: JsActionType, keysToSend: Option[String], conditions: Seq[JsCondition]) extends WebTask {
 
   override def maxPoints: Points = (1 point) + (conditions.size points)
@@ -91,17 +92,6 @@ final case class JsTask(id: Int, text: String, xpathQuery: String, actionType: J
   }
 
 }
-
-class WebExTag(part: String, hasExes: Boolean) extends ExTag {
-
-  override def cssClass: String = if (hasExes) "label label-primary" else "label label-default"
-
-  override def buttonContent: String = part
-
-  override def title: String = s"Diese Aufgabe besitzt ${if (!hasExes) "k" else ""}einen $part-Teil"
-
-}
-
 
 final case class HtmlAttribute(key: String, value: String)
 
