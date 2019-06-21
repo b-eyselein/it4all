f9a5ac28a078b721cebaa019142041d655d2d09a
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2019-06-21 14:09:47 +0200

    Remove config param maxConns / maxThreads for slick

--- app/model/tools/bool/BoolSolutionJsonFormat.scala
+++ app/model/tools/bool/BoolSolutionJsonFormat.scala
@@ -18,7 +18,7 @@ object BoolSolutionJsonFormat {
       (__ \ valueName).read[Boolean]
     ) { (varStr, value) => BoolAssignment.apply(Variable(varStr(0)), value) }
 
-  private implicit val boolTableRowReads: Reads[BoolTableRow] = (JsPath \ assignmentsName).read[Seq[BoolAssignment]] map BoolTableRow.fromAssignments
+  private implicit val boolTableRowReads: Reads[BoolTableRow] = (JsPath \ assignmentsName).read[Seq[BoolAssignment]].map(BoolTableRow.fromAssignments)
 
   val boolSolutionReads: Reads[BoolSolution] = (
     (__ \ formulaName).read[String] and
