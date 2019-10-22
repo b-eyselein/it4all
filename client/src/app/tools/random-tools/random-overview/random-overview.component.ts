e475dbdcd05ce58e56c5409ed8ced05b74933768
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2019-10-22 09:43:30 +0200

    Small design updates client

--- client/src/app/tools/random-tools/random-overview/random-overview.component.ts
+++ client/src/app/tools/random-tools/random-overview/random-overview.component.ts
@@ -3,23 +3,7 @@ import {ActivatedRoute} from '@angular/router';
 import {Tool} from '../../../_interfaces/tool';
 import {randomTools} from '../random-tools-list';
 
-@Component({
-  // selector: 'app-random-overview',
-  template: `
-      <div class="container">
-          <h1 class="title is-3 has-text-centered">Übungsaufgaben</h1>
-          <div class="buttons">
-              <ng-container *ngFor="let part of tool.parts">
-                  <a *ngIf="!part.disabled" [routerLink]="['/randomTools', tool.id, part.id]"
-                     class="button is-link is-fullwidth">{{part.name}}</a>
-              </ng-container>
-          </div>
-
-          <h1 *ngIf="tool.hasPlayground" class="title is-3 has-text-centered">Playground</h1>
-
-          <h1 *ngIf="false" class="title is-3 has-text-centered">Evaluation</h1>
-      </div>`,
-})
+@Component({templateUrl: './random-overview.component.html'})
 export class RandomOverviewComponent implements OnInit {
 
    tool: Tool;
