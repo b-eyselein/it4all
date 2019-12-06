8558e89ffa8c6a26636bc5d7dd4ba822338f9795
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2019-12-06 13:47:34 +0100

    Updates for prod mode

--- client/src/app/admin/admin-read-collections/read-collection/read-collection.component.ts
+++ client/src/app/admin/admin-read-collections/read-collection/read-collection.component.ts
@@ -2,7 +2,10 @@ import {Component, Input} from '@angular/core';
 import {ApiService} from '../../../tools/collection-tools/_services/api.service';
 import {IExerciseCollection} from '../../../_interfaces/models';
 
-@Component({selector: 'it4all-read-collection', templateUrl: './read-collection.component.html'})
+@Component({
+  selector: 'it4all-read-collection',
+  templateUrl: './read-collection.component.html'
+})
 export class ReadCollectionComponent {
 
   @Input() collection: IExerciseCollection;
