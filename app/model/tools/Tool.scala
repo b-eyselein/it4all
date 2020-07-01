bc8f4b8decd3bf9bbc7219a21baa55874842c398
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2020-07-01 12:35:30 +0200

    small refactoring

---
+++ app/model/tools/Tool.scala
@@ -0,0 +1,64 @@
+package model.tools
+
+import better.files.File
+import enumeratum.{EnumEntry, PlayEnum}
+import initialData.InitialData
+import model._
+import model.core.result.AbstractCorrectionResult
+import model.graphql.ToolGraphQLModelBasics
+
+import scala.concurrent.{ExecutionContext, Future}
+
+// Tool states
+
+sealed trait ToolState extends EnumEntry
+
+object ToolState extends PlayEnum[ToolState] {
+
+  override val values: IndexedSeq[ToolState] = findValues
+
+  case object PRE_ALPHA extends ToolState
+
+  case object ALPHA extends ToolState
+
+  case object BETA extends ToolState
+
+  case object LIVE extends ToolState
+
+}
+
+// Tools
+
+abstract class Tool(val id: String, val name: String, val toolState: ToolState = ToolState.LIVE) {
+
+  // Abstract types
+
+  type SolType
+  type ExContentType <: ExerciseContent
+  type PartType <: ExPart
+  type ResType <: AbstractCorrectionResult
+
+  // Yaml, Html forms, Json, GraphQL
+
+  val jsonFormats: ToolJsonProtocol[SolType, ExContentType, PartType]
+
+  val graphQlModels: ToolGraphQLModelBasics[SolType, ExContentType, PartType, ResType]
+
+  // Other helper methods
+
+  def solutionDirForExercise(username: String, collId: Int, exId: Int): File =
+    File.currentWorkingDirectory / "data" / id / "solutions" / username / s"$collId" / s"$exId"
+
+  def correctAbstract(
+    user: LoggedInUser,
+    solution: SolType,
+    exercise: Exercise[ExContentType],
+    part: PartType,
+    solutionSaved: Boolean
+  )(implicit executionContext: ExecutionContext): Future[ResType]
+
+  val allTopics: Seq[Topic] = Seq.empty
+
+  val initialData: InitialData[ExContentType]
+
+}
