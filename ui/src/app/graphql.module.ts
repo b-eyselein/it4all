ac0fcc12e08f9b658e9176db539ef15ee8e8fb8a
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2020-07-31 12:17:46 +0200

    reactivate fragment matcher

--- ui/src/app/graphql.module.ts
+++ ui/src/app/graphql.module.ts
@@ -1,14 +1,19 @@
 import {NgModule} from '@angular/core';
 import {APOLLO_OPTIONS, ApolloModule} from 'apollo-angular';
 import {HttpLink, HttpLinkModule} from 'apollo-angular-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import {InMemoryCache, IntrospectionFragmentMatcher} from 'apollo-cache-inmemory';
+import possibleTypes from '../introspection-result';
 
 const uri = '/api/graphql'; // <-- add the URL of the GraphQL server here
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData: possibleTypes
+});
 
 export function createApollo(httpLink: HttpLink) {
   return {
     link: httpLink.create({uri}),
-    cache: new InMemoryCache()
+    cache: new InMemoryCache({fragmentMatcher})
   };
 }
 
