37f226a9f22be827f0c2c878843ecef6a6dd38b1
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2022-11-02 10:58:07 +0100

    fixes for prod mode

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -102,14 +102,6 @@ create table if not exists user_solutions_with_parts (
     on update cascade on delete cascade
 );
 
--- grant privileges...
-
-grant all privileges on all tables in schema public to it4all_admin;
-grant all privileges on all sequences in schema public to it4all_admin;
-
-grant select, update, insert, delete on all tables in schema public to it4all;
-grant select, usage on all sequences in schema public to it4all;
-
 -- !Downs
 
 drop table if exists user_solutions_with_parts;
