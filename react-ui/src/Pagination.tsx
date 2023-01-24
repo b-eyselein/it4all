ca1cd2c6e0895ac520dbf37c7080c37bc32864f4
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2023-01-24 11:39:31 +0100

    fix style pagination

--- react-ui/src/Pagination.tsx
+++ react-ui/src/Pagination.tsx
@@ -13,18 +13,16 @@ export function Pagination({pages, maxPage, currentPage, setCurrentPage}: IProps
   const {t} = useTranslation('common');
 
   return (
-    <nav className="pagination is-centered" role="navigation" aria-label="pagination">
-      <button className="button pagination-previous" onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage < 1}>
+    <nav className="my-4 flex justify-center">
+      <button className="mx-1 px-4 py-2 rounded border border-slate-500" onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage < 1}>
         {t('previous')}
       </button>
-      <ul className="pagination-list">
-        {pages.map((page) => <li key={page}>
-          <button className={classNames('button', 'pagination-link', {'is-current': page === currentPage})} onClick={() => setCurrentPage(page)}>
-            {page}
-          </button>
-        </li>)}
-      </ul>
-      <button className="button pagination-next" onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage >= maxPage - 1}>
+      {pages.map((page) =>
+        <button key={page} className={classNames('mx-1 px-4 py-2 rounded', page === currentPage ? 'bg-blue-600 text-white' : 'border border-slate-500')}
+                onClick={() => setCurrentPage(page)}>
+          {page}
+        </button>)}
+      <button className="mx-1 px-4 py-2 rounded border border-slate-500" onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage >= maxPage - 1}>
         {t('next')}
       </button>
     </nav>
